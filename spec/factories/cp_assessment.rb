284382d810f660ddd4f1eeff183b221eb70a1b13
Tomasz Subik <tsubik@gmail.com>
2019-07-15 19:12:12 +0200

    import location legislative process, fix some imports

---
+++ spec/factories/cp_assessment.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :cp_assessment, class: CP::Assessment do
+    association :company
+
+    assessment_date { 1.year.ago }
+    publication_date { 11.months.ago }
+
+    level { '1' }
+    assumptions { 'Assumptions about the assessment' }
+
+    emissions do
+      (2013..2030).map do |year|
+        {year => rand(120..140)}
+      end.reduce(&:merge)
+    end
+  end
+end
