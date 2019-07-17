35427f2fba13e1b6cbf172ea27680295cde5a3d0
Tomasz Subik <tsubik@gmail.com>
2019-07-17 13:17:41 +0200

    cp assessments controller spec

---
+++ spec/controllers/admin/cp_assessments_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Admin::CpAssessmentsController, type: :controller do
+  let(:admin) { create(:admin_user) }
+  let!(:cp_assessment) { create(:cp_assessment) }
+
+  before { sign_in admin }
+
+  describe 'GET index' do
+    subject { get :index }
+
+    it { is_expected.to be_successful }
+  end
+
+  describe 'GET show' do
+    subject { get :show, params: {id: cp_assessment.id} }
+
+    it { is_expected.to be_successful }
+  end
+end
