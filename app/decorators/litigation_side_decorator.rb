5c5b83f2dab7c119a6c16c05504f95b80c8a5844
Tomasz Subik <tsubik@gmail.com>
2019-07-24 20:03:21 +0200

    update litigation side decorator

--- app/decorators/litigation_side_decorator.rb
+++ app/decorators/litigation_side_decorator.rb
@@ -1,6 +1,13 @@
 class LitigationSideDecorator < Draper::Decorator
   delegate_all
 
+  def name
+    return h.link_to company.name, h.admin_company_path(company) if company?
+    return h.link_to location.name, h.admin_location_path(location) if location?
+
+    model.name
+  end
+
   def party_type
     model.party_type&.humanize
   end
