3d99501aa9665ea58a8cc192e5ec8f3a2050a515
Tomasz Subik <tsubik@gmail.com>
2019-07-25 13:38:40 +0200

    litigation sides: nice default select

--- app/javascript/controllers/litigation_side_controller.js
+++ app/javascript/controllers/litigation_side_controller.js
@@ -1,15 +1,30 @@
 import { Controller } from 'stimulus';
 
+const ENTITY_TYPE_PARTY_TYPE_MAP = {
+  'Company': 'corporation',
+  'Location': 'government'
+};
+
 export default class extends Controller {
   connect() {
-    $(this.connectedWithSelect).on('change', this._setDefaultName.bind(this));
+    $(this.connectedWithSelect).on('change', this._setDefaults.bind(this));
   }
 
-  _setDefaultName() {
-    const selectedOptions = this.connectedWithSelect.selectedOptions;
+  _setDefaults() {
+    const selectedOption = this.connectedWithSelect.selectedOptions &&
+          this.connectedWithSelect.selectedOptions.length &&
+          this.connectedWithSelect.selectedOptions[0];
 
-    if (selectedOptions.length) {
-      this.nameInput.value = selectedOptions[0].text;
+    if (selectedOption) {
+      const entityType = selectedOption.value.split('-')[0];
+      const partyType = ENTITY_TYPE_PARTY_TYPE_MAP[entityType];
+
+      if (partyType) {
+        $(this.partyTypeSelect).val(partyType);
+        $(this.partyTypeSelect).trigger('change');
+      }
+
+      this.nameInput.value = selectedOption.text;
     }
   }
 
@@ -19,5 +34,9 @@ export default class extends Controller {
 
   get nameInput() {
     return this.element.querySelector('input[id*="_name"');
+  }
+
+  get partyTypeSelect() {
+    return this.element.querySelector('select[id*="_party_type"');
   }
 }
