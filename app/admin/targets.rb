4db0a88860c23044501ec932a6122f06f5af3b11
Tomasz Subik <tsubik@gmail.com>
2019-07-29 14:38:06 +0200

    adding Targets to admin panel

---
+++ app/admin/targets.rb
@@ -0,0 +1,40 @@
+ActiveAdmin.register Target do
+  menu parent: 'Laws', priority: 3
+
+  permit_params :description, :sector_id, :location_id, :legislation_id,
+                :year, :type, :base_year_period, :ghg_target
+
+  filter :type,
+         as: :check_boxes,
+         collection: proc { array_to_select_collection(Target::TYPES) }
+  filter :ghg_target
+  filter :sector
+
+  config.batch_actions = false
+
+  index do
+    selectable_column
+    column :location
+    column :sector
+    column :ghg_target
+    column :year
+    actions
+  end
+
+  form do |f|
+    f.semantic_errors(*f.object.errors.keys)
+
+    f.inputs do
+      f.input :type, as: :select, collection: array_to_select_collection(Target::TYPES)
+      f.input :ghg_target
+      f.input :year
+      f.input :base_year_period
+      f.input :location
+      f.input :sector
+      f.input :legislation
+      f.input :description, as: :trix
+    end
+
+    f.actions
+  end
+end
---
+++ spec/controllers/admin/targets_controller_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+RSpec.describe Admin::TargetsController, type: :controller do
+  let(:admin) { create(:admin_user) }
+  let!(:target) { create(:target) }
+  let(:sector) { create(:sector) }
+  let(:location) { create(:location) }
+  let(:legislation) { create(:legislation) }
+  let(:valid_attributes) {
+    attributes_for(
+      :target,
+      legislation_id: legislation.id,
+      location_id: location.id,
+      sector_id: sector.id
+    )
+  }
+  let(:invalid_attributes) { valid_attributes.merge(type: nil) }
+
+  before { sign_in admin }
+
+  describe 'GET index' do
+    subject { get :index }
+
+    it { is_expected.to be_successful }
+  end
+
+  describe 'GET show' do
+    subject { get :show, params: {id: target.id} }
+
+    it { is_expected.to be_successful }
+  end
+
+  describe 'GET new' do
+    subject { get :new }
+
+    it { is_expected.to be_successful }
+  end
+
+  describe 'GET edit' do
+    subject { get :edit, params: {id: target.id} }
+
+    it { is_expected.to be_successful }
+  end
+
+  describe 'POST create' do
+    context 'with valid params' do
+      subject { post :create, params: {target: valid_attributes} }
+
+      it 'creates a new Target' do
+        expect { subject }.to change(Target, :count).by(1)
+      end
+
+      it 'redirects to the created Target' do
+        expect(subject).to redirect_to(admin_target_path(Target.order(:created_at).last))
+      end
+    end
+
+    context 'with invalid params' do
+      subject { post :create, params: {target: invalid_attributes} }
+
+      it { is_expected.to be_successful }
+
+      it 'invalid_attributes do not create a Target' do
+        expect { subject }.not_to change(Target, :count)
+      end
+    end
+  end
+end
