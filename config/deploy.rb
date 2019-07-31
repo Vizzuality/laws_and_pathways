ef19c2f8bd8bc0a6730f9fc2a24c2b2cd2738c47
Tiago Garcia <tiagojsag@gmail.com>
2019-07-31 18:08:52 +0200

    Capistrano settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/var/www/laws-pathways"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, '.env'
+append :linked_files, '.env', 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/downloads', 'vendor/bundle'
