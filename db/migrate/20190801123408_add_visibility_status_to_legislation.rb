417acfa80a213af4e657b66d8de4b2b34557b6f2
Marek Kowalcze <marek.kowalcze@gmail.com>
2019-08-02 11:30:45 +0200

    add index

--- db/migrate/20190801123408_add_visibility_status_to_legislation.rb
+++ db/migrate/20190801123408_add_visibility_status_to_legislation.rb
@@ -1,5 +1,5 @@
 class AddVisibilityStatusToLegislation < ActiveRecord::Migration[5.2]
   def change
-    add_column :legislations, :visibility_status, :string
+    add_column :legislations, :visibility_status, :string, index: true
   end
 end
