bc3adff0ed4b42aa6b245756b27ffadfa45f7b5e
Tomasz Subik <tsubik@gmail.com>
2019-08-06 15:54:31 +0200

    capistrano link credentials file

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,3 +2,5 @@ server '35.242.181.47', user: 'ubuntu', roles: %w{web app db}, primary: true
 set :ssh_options, forward_agent: true
 
 set :branch, 'develop'
+
+append :linked_files, 'config/secrets/google-credentials-staging.json'
