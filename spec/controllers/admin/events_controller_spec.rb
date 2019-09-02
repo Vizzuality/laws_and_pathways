7746bb911401617adbf2cd7b49f3ea249fc81c0d
Marek Kowalcze <marek.kowalcze@gmail.com>
2019-09-02 11:52:29 +0200

    specs for events controller

---
+++ spec/controllers/admin/events_controller_spec.rb
@@ -0,0 +1,105 @@
+require 'rails_helper'
+require 'csv'
+
+RSpec.describe Admin::EventsController, type: :controller do
+  let(:admin) { create(:admin_user) }
+  let!(:legislation1) { create(:legislation, visibility_status: 'published') }
+  let!(:legislation2) { create(:legislation, visibility_status: 'pending') }
+  let!(:litigation1) { create(:litigation, visibility_status: 'published') }
+  let!(:litigation2) { create(:litigation, visibility_status: 'draft') }
+
+  let!(:event1) { create(:legislation_event, title: 'Event1', eventable: legislation1) } # with published documentable
+  let!(:event2) { create(:legislation_event, title: 'Event2', eventable: legislation2) }
+  let!(:event3) { create(:litigation_event, title: 'Event3', eventable: litigation1) } # with published documentable
+  let!(:event4) { create(:litigation_event, title: 'Event4', eventable: litigation2) }
+
+  before { sign_in admin }
+
+  describe 'GET index' do
+    subject { get :index }
+
+    it { is_expected.to be_successful }
+  end
+
+  describe 'GET index with .csv format' do
+    before :each do
+      get :index, format: 'csv'
+    end
+
+    it('returns CSV file') do
+      expect(response.header['Content-Type']).to include('text/csv')
+    end
+
+    it('returns all events') do
+      csv = response_as_csv
+
+      expect(csv.by_col[4].sort).to eq(%w[Event1 Event2 Event3 Event4])
+    end
+  end
+
+  describe 'GET index with .csv format (query, results present)' do
+    before :each do
+      get :index,
+          params: {
+            q: {
+              # should narrow down results to 'Event1'
+              eventable: {visibility_status_eq: 'published'},
+              eventable_type_eq: 'Legislation'
+            }
+          },
+          format: 'csv'
+    end
+
+    it('returns CSV file') do
+      expect(response.header['Content-Type']).to include('text/csv')
+    end
+
+    it('returns filtered events list') do
+      csv = response_as_csv
+
+      # returned CSV must contain only result row + header
+      expect(csv.to_a.size).to eq(2)
+
+      # check filtered data
+      expect(csv[0]['Title']).to eq(event1.title)
+      expect(csv[0]['Description']).to eq(event1.description)
+      expect(csv[0]['Event type']).to eq(event1.event_type)
+
+      # only single data row
+      expect(csv[1]).to be_nil
+    end
+  end
+
+  describe 'GET index with .csv format (query, no results)' do
+    before :each do
+      get :index,
+          params: {
+            q: {
+              # should return no results for that query!
+              eventable: {visibility_status_eq: 'pending'},
+              eventable_type_eq: 'Litigation'
+            }
+          },
+          format: 'csv'
+    end
+
+    it('returns CSV file') do
+      expect(response.header['Content-Type']).to include('text/csv')
+    end
+
+    it('returns empty CSV file (only header)') do
+      csv = response_as_csv
+
+      # only header expected
+      expected_columns = [
+        'Id', 'Eventable type', 'Eventable', 'Event type',
+        'Title', 'Description', 'Date', 'Url'
+      ]
+      expect(csv.to_a).to eq([expected_columns])
+    end
+  end
+
+  def response_as_csv
+    CSV.parse(response.body, headers: true)
+  end
+end
