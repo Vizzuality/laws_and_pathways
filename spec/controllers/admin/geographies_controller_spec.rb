f1cb444702ce3302ea32b64b7dfc37f8d75f088d
Marek Kowalcze <marek.kowalcze@gmail.com>
2019-09-05 15:08:52 +0200

    controller tests updated

--- spec/controllers/admin/geographies_controller_spec.rb
+++ spec/controllers/admin/geographies_controller_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe Admin::GeographiesController, type: :controller do
   let(:admin) { create(:admin_user) }
   let!(:geography) { create(:geography) }
-  let(:valid_attributes) { attributes_for(:geography) }
-  let(:invalid_attributes) { valid_attributes.merge(iso: nil) }
 
   before { sign_in admin }
 
@@ -34,10 +32,45 @@ RSpec.describe Admin::GeographiesController, type: :controller do
 
   describe 'POST create' do
     context 'with valid params' do
-      subject { post :create, params: {geography: valid_attributes} }
+      let(:valid_params) do
+        attributes_for(
+          :geography,
+          iso: 'ABC',
+          name: 'Test Country',
+          region: Geography::REGIONS.last,
+          visibility_status: 'pending',
+          indc_url: 'https://example.indc.pl',
+          federal: true,
+          federal_details: 'federal details text',
+          legislative_process: 'legislative process text',
+          events_attributes: [
+            {
+              date: 5.months.ago,
+              event_type: 'election',
+              title: 'president elections',
+              description: 'Description of elections'
+            }
+          ]
+        )
+      end
+
+      subject { post :create, params: {geography: valid_params} }
 
       it 'creates a new Geography' do
         expect { subject }.to change(Geography, :count).by(1)
+
+        last_geography_created.tap do |g|
+          expect(g.iso).to eq(valid_params[:iso])
+          expect(g.name).to eq(valid_params[:name])
+          expect(g.region).to eq(valid_params[:region])
+          expect(g.visibility_status).to eq(valid_params[:visibility_status])
+          expect(g.indc_url).to eq(valid_params[:indc_url])
+          expect(g.federal).to eq(valid_params[:federal])
+          expect(g.federal_details).to eq(valid_params[:federal_details])
+          expect(g.legislative_process).to eq(valid_params[:legislative_process])
+          expect(g.events.pluck(:title, :event_type, :description))
+            .to eq(valid_params[:events_attributes].pluck(:title, :event_type, :description))
+        end
       end
 
       it 'redirects to the created Geography' do
@@ -46,7 +79,9 @@ RSpec.describe Admin::GeographiesController, type: :controller do
     end
 
     context 'with invalid params' do
-      subject { post :create, params: {geography: invalid_attributes} }
+      let(:invalid_params) { attributes_for(:geography, iso: nil) }
+
+      subject { post :create, params: {geography: invalid_params} }
 
       it { is_expected.to be_successful }
 
@@ -54,5 +89,9 @@ RSpec.describe Admin::GeographiesController, type: :controller do
         expect { subject }.not_to change(Geography, :count)
       end
     end
+  end
+
+  def last_geography_created
+    Geography.order(:created_at).last
   end
 end
