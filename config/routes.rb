beeb81504d4097326d26f9f654a9c54fa5e7be2d
Marek Kowalcze <marek.kowalcze@gmail.com>
2019-09-13 16:41:52 +0200

    [TPI frontend] Sectors dashboard PoC with chartkick

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'companies/show'
+  get 'sectors/index'
+  get 'sectors/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
