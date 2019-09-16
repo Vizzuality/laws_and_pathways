ba1ce9f0e1328d35a176368f9345c6f353de1e32
Marek Kowalcze <marek.kowalcze@gmail.com>
2019-09-16 13:50:02 +0200

    use real data on CP perf. chart for sector; add tpi stylesheets

--- app/controllers/sectors_controller.rb
+++ app/controllers/sectors_controller.rb
@@ -2,31 +2,27 @@ class SectorsController < ApplicationController
   CP_ALIGNMENTS = %w[below_2 exact_2 paris not_aligned no_disclosure].freeze
 
   def index
-    @companies_data = get_companies_grouped_by_sector_levels(Company)
+    @companies_by_levels = get_companies_grouped_by_sector_levels(Company)
 
     @sectors_cp_data = CP_ALIGNMENTS.map do |cp_alignment| # CP alignments are series
-      {name: get_alignment_label(cp_alignment), data: get_companies_count_for_cp_alignment(cp_alignment)}
+      {
+        name: get_alignment_label(cp_alignment),
+        data: get_companies_count_for_cp_alignment(cp_alignment)
+      }
     end
   end
 
   def show
     @sector = Sector.find(params[:id])
 
-    @companies_data = get_companies_grouped_by_sector_levels(@sector.companies)
+    @companies_by_levels = get_companies_grouped_by_sector_levels(@sector.companies)
   end
 
-  # get companies grouped by latest MQ assessments sector's levels
+  # get companies grouped by their latest MQ assessments sector's levels
   def get_companies_grouped_by_sector_levels(company_scope)
     company_scope
       .includes(:mq_assessments)
       .group_by { |company| company.mq_assessments.order(:assessment_date).first.level }
-      .map { |sector_level, companies| ["Level #{sector_level}", companies] }
-  end
-
-  def get_companies_count_for_cp_alignment(_cp_alignment)
-    Sector.take(10).pluck(:id, :name).map do |_sector_id, sector_name|
-      [sector_name, rand(100)]
-    end
   end
 
   def get_alignment_label(cp_alignment)
@@ -37,5 +33,11 @@ class SectorsController < ApplicationController
       not_aligned: 'Not aligned',
       no_disclosure: 'No disclosure'
     }[cp_alignment.to_sym]
+  end
+
+  def get_companies_count_for_cp_alignment(_cp_alignment)
+    Sector.take(10).pluck(:id, :name).map do |_sector_id, sector_name|
+      [sector_name, rand(100)]
+    end
   end
 end
