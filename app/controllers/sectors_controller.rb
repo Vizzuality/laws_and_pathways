c2bb2fe72c8e2bdb920a22c263e77eb0bb922d7b
Marek Kowalcze <marek.kowalcze@gmail.com>
2019-09-17 20:59:49 +0200

    better comments

--- app/controllers/sectors_controller.rb
+++ app/controllers/sectors_controller.rb
@@ -10,9 +10,14 @@ class SectorsController < ApplicationController
     @companies_by_levels = companies_grouped_by_sector_levels(companies_scope(params))
   end
 
-  # chart data endpoints
+  # Chart data endpoints
 
-  # Returns data:
+  # Returns array of pairs:
+  #   [latest MQ assessment Sector's level, number of Companies on given level]
+  #
+  # If params[:id] is given results are scoped to given Sector.
+  #
+  # @example:
   #   [
   #     ['0', 13],
   #     ['1', 63],
@@ -21,6 +26,7 @@ class SectorsController < ApplicationController
   #     ['4', 63],
   #     ['4STAR', 6]
   #   ]
+  #
   def companies_levels
     data = companies_grouped_by_sector_levels(companies_scope(params))
       .map { |level, companies| [level, companies.size] }
@@ -28,11 +34,19 @@ class SectorsController < ApplicationController
     render json: data.chart_json
   end
 
-  # [
-  #   { name: 'WizzAir', data: {} },
-  #   { name: 'Air China', data: {'2014' => 111.0, '2015' => 112.0 } },
-  #   { name: 'China Southern', data: {'2014' => 114.0, '2015' => 112.0 } }
-  # ]
+  # Returns array of objects with props:
+  # - name - Company name
+  # - data - object representing emissions history ({ year: emission-value })
+  #
+  # If params[:id] is given results are scoped to given Sector.
+  #
+  # @example
+  #   [
+  #     { name: 'WizzAir', data: {} },
+  #     { name: 'Air China', data: {'2014' => 111.0, '2015' => 112.0 } },
+  #     { name: 'China Southern', data: {'2014' => 114.0, '2015' => 112.0 } }
+  #   ]
+  #
   def companies_emissions
     data = companies_grouped_by_sector_levels(companies_scope(params))
       .map { |_level, companies| companies }
@@ -45,39 +59,10 @@ class SectorsController < ApplicationController
     render json: sectors_scenarios_alignments.chart_json
   end
 
-  # Returns info of how many companies from each sector are in given CP scenario group
-  #
-  # [
-  #  {
-  #   name: 'Below 2',
-  #   data: [ ['Coal Mining', 52], ['Steel', 73] ]
-  #  },
-  #  {
-  #    name: 'Paris',
-  #    data: [ ['Coal Mining', 65], ['Steel', 26] ]
-  #  }
-  # ]
   def sectors_scenarios_alignments
     ::Api::Sectors::CompaniesCountGroupedByScenario.new.get
   end
 
-  # Returns companies grouped by their latest MQ assessments sector's levels
-  #
-  # [
-  #  ['1',
-  #    [
-  #      { name: 'Air China', emissions: { 2013: 153, 2014: 142 } },
-  #      { name: 'China Southern', emissions: { 2015: 32, 2016: 43 } }
-  #    ]
-  #  ],
-  #  ['3',
-  #    [
-  #      { name: 'Alaska Air', emissions: { } },
-  #      { name: 'IAG', emissions: { } }
-  #      { name: 'Japan Airlines', emissions: { } }
-  #    ]
-  #   ]
-  # ]
   def companies_grouped_by_sector_levels(company_scope)
     ::Api::Sectors::CompaniesNamesGroupedByLevel.new(company_scope).get
   end
