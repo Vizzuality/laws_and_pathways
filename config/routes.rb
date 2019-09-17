7fcf8624c2df630a187defff8ffdbff079cae061
Marek Kowalcze <marek.kowalcze@gmail.com>
2019-09-17 20:34:20 +0200

    dedicated endpoints for charts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # TPI
-  resources :sectors, only: [:show, :index]
+  resources :sectors, only: [:show, :index] do
+    collection do
+      get :companies_levels
+      get :scenarios
+    end
+    member do
+      get :companies_levels
+      get :companies_emissions
+    end
+  end
   resources :companies, only: [:show]
 
   devise_for :admin_users, ActiveAdmin::Devise.config
