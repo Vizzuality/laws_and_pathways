bb6f04bd6abef139a13abc1195748d1ed99a162d
Tomasz Subik <tsubik@gmail.com>
2019-09-18 16:05:46 +0200

    create HasEmissions concern

--- app/models/cp/assessment.rb
+++ app/models/cp/assessment.rb
@@ -14,17 +14,12 @@
 
 module CP
   class Assessment < ApplicationRecord
+    include HasEmissions
+
     belongs_to :company
 
     scope :latest_first, -> { order(assessment_date: :desc) }
 
     validates_presence_of :publication_date
-
-    # emissions is a hash like { year => value }, this method returns all years
-    def emissions_all_years
-      return [] unless emissions.present?
-
-      emissions.keys
-    end
   end
 end
--- app/models/cp/benchmark.rb
+++ app/models/cp/benchmark.rb
@@ -13,30 +13,13 @@
 
 module CP
   class Benchmark < ApplicationRecord
+    include HasEmissions
+
     belongs_to :sector
 
     scope :latest_first, -> { order(release_date: :desc) }
     scope :by_release_date, -> { order(:release_date) }
 
     validates_presence_of :release_date, :scenario
-
-    def emissions_all_years
-      emissions.keys
-    end
-
-    def average_emission
-      none_empty_values = emissions.values.compact
-      return 0 if none_empty_values.empty?
-
-      none_empty_values.map(&:to_f).reduce(:+) / none_empty_values.length
-    end
-
-    def emissions=(value)
-      if value.is_a?(String)
-        write_attribute(:emissions, JSON.parse(value))
-      else
-        super
-      end
-    end
   end
 end
