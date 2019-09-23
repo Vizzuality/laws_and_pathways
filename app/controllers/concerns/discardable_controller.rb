b76ae8a1d0ff316d799e96ed0c43f8edff29809e
Adam Gwozdowski <adam99g@gmail.com>
2019-09-23 10:46:01 +0200

    Added InvalidDestroyCommandError when destroy command not exist

--- app/controllers/concerns/discardable_controller.rb
+++ app/controllers/concerns/discardable_controller.rb
@@ -1,16 +1,25 @@
 module DiscardableController
+  InvalidDestroyCommandError = Class.new(StandardError)
+
   # Override default Active Admin destroy method using Discardable Model.
   # It sets discarded_at for entity and for its dependent resources.
   # Redirect to resource list with proper message
   def destroy
     entity_name = ActiveSupport::Inflector.singularize(controller_name).camelize
-    entity_destroy_class = "::Command::Destroy::#{entity_name}".constantize
-    destroy_command = entity_destroy_class.new(resource.object)
+    destroy_command = entity_destroy_class(entity_name).new(resource.object)
 
     success_message = {notice: "Successfully deleted selected #{entity_name}"}
     alert_message = {alert: "Could not delete selected #{entity_name}"}
     message = destroy_command.call ? success_message : alert_message
 
     redirect_to send("admin_#{controller_name}_path"), message
+  end
+
+  private
+
+  def entity_destroy_class(entity_name)
+    "::Command::Destroy::#{entity_name}".constantize
+  rescue NameError
+    raise InvalidDestroyCommandError, "Destroy command for #{entity_name} class does not exist"
   end
 end
