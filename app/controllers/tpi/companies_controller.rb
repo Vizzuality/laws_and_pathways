d6efe5a87f749cd7cd33b5cd2bc8163bb816f771
Marek Kowalcze <marek.kowalcze@gmail.com>
2019-09-23 12:58:39 +0200

    move everything under :tpi namespace

---
+++ app/controllers/tpi/companies_controller.rb
@@ -0,0 +1,21 @@
+module Tpi
+  class CompaniesController < ApplicationController
+    def show
+      @company = Company.find(params[:id])
+
+      @company_details = ::Api::Presenters::Company.new(@company).company_details
+    end
+
+    # Data:     Company emissions
+    # Section:  CP
+    # Type:     line chart
+    # On pages: :show
+    def emissions_chart_data
+      @company = Company.find(params[:id])
+
+      data = ::Api::Charts::Company.new(@company).emissions_data
+
+      render json: data.chart_json
+    end
+  end
+end
---
+++ app/controllers/tpi/sectors_controller.rb
@@ -0,0 +1,56 @@
+module Tpi
+  class SectorsController < ApplicationController
+    def index
+      @sectors_names = Sector.order(:name).pluck(:id, :name)
+      @companies_names = Company.limit(50).pluck(:id, :name)
+    end
+
+    def show
+      @sector = Sector.find(params[:id])
+
+      @companies_by_levels = ::Api::Charts::Sector.new(companies_scope(params)).companies_summaries
+    end
+
+    # Chart data endpoints
+
+    # Data:     Sectors Companies number, grouped by Levels
+    # Section:  MQ
+    # Type:     pie chart
+    # On pages: :index, :show
+    def levels_chart_data
+      data = ::Api::Charts::Sector.new(companies_scope(params)).companies_count
+
+      render json: data.chart_json
+    end
+
+    # Data:     Companies emissions
+    # Section:  CP
+    # Type:     line chart
+    # On pages: :show
+    def emissions_chart_data
+      data = ::Api::Charts::Sector.new(companies_scope(params)).companies_emissions_data
+
+      render json: data.chart_json
+    end
+
+    # Data:     Sector Companies numbers, grouped by CP Benchmarks from given Sector
+    # Section:  CP
+    # Type:     column chart
+    # On pages: :index
+    def benchmarks_chart_data
+      data = ::Api::Charts::Sector.new(companies_scope(params)).group_by_cp_benchmark
+
+      render json: data.chart_json
+    end
+
+    private
+
+    def companies_scope(params)
+      if params[:id]
+        Sector.find(params[:id]).companies
+      else
+        Company
+      end
+    end
+  end
+end
