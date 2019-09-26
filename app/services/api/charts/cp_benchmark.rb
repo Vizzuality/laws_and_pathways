a92c4ce102ad05a6a97bd6ead1cf4c8fdd690b1e
Marek Kowalcze <marek.kowalcze@gmail.com>
2019-09-26 14:49:09 +0200

    WIP: less db queries

--- app/services/api/charts/cp_benchmark.rb
+++ app/services/api/charts/cp_benchmark.rb
@@ -15,97 +15,50 @@ module Api
       #     }
       #   ]
       def cp_performance
-        prepare_chart_data(sectors_cp_performance)
+        ::Company
+          .includes(:cp_assessments, sector: [:cp_benchmarks])
+          .group_by { |company| company_current_scenario(company) }
+          .map do |scenario, companies|
+            {
+              name: scenario,
+              data: companies_count_by_sector(companies)
+            }
+          end
       end
 
       private
 
-      # Prepare data for chart based on sectors cp performance data
-      # @param results[Array<Hash>] list with all scenarios with emissions
-      # @return [Array<Hash>] list with data for chart
-      def prepare_chart_data(results)
-        results.map do |scenario, companies|
-          data = companies.map do |company, value|
-            [company, value]
-          end
-
-          {name: scenario, data: data}
-        end
+      def companies_count_by_sector(companies)
+        companies
+          .group_by {|c| c.sector.name }
+          .map {|sector_name, companies| [sector_name, companies.size] }
       end
 
-      # Scenarios with sectors and companies count
-      # @return [Hash]
-      # @example
-      #   {
-      #     "2 Degrees (High Efficiency)" => {
-      #       "Aluminium" => 10,
-      #       "Cement" => 27
-      #     },
-      #       "International Pledges" => {
-      #       "Steel" => 80
-      #     }
-      #   }
-      def sectors_cp_performance
-        results = {}
+      def company_current_scenario(company)
+        company_last_emission = company_last_reported_emission(company)
 
-        sectors_scenario_emissions.each do |sector, scenarios|
-          sector.companies.each do |company|
-            next if scenarios.empty?
+        return nil unless company_last_emission
 
-            company_emission = company_emission(company)
+        sector_last_reported_emissions = company
+          .sector
+          .cp_benchmarks
+          .map { |cp| [cp.scenario, cp.emissions[current_year]] }
 
-            next unless company_emission
-
-            company_scenario = company_scenario(company_emission, scenarios)
-
-            results[company_scenario] ||= Hash.new(0)
-            results[company_scenario][sector.name] += 1
-          end
-        end
-
-        results
-      end
-
-      # Get emissions for all scenarios for all sectors
-      # @return [Hash]
-      # @example
-      #   {"Airlines": {"Below 2 Degrees": 110, "2 Degrees": 101}}
-      def sectors_scenario_emissions
-        sectors = {}
-
-        ::Sector.all.each do |sector|
-          sectors[sector] = {}
-          sector.cp_benchmarks.each do |benchmark|
-            sectors[sector][benchmark.scenario] = benchmark.emissions[current_year]
-          end
-        end
-
-        sectors
+        sector_last_reported_emissions
+          .min_by { |_sector, sector_emission| (sector_emission - company_last_emission).abs }
+          .first
       end
 
       # Get company emission for current year or for the latest assessment
       # @param company [Company]
       # @return [Float]
-      def company_emission(company)
-        emissions = company.cp_assessments.order(:assessment_date).last&.emissions
+      def company_last_reported_emission(company)
+        company_all_emissions = company.cp_assessments.order(:assessment_date).last&.emissions
+        return if company_all_emissions.blank?
 
-        return if emissions.blank?
+        company_last_reported_year = current_year
 
-        # Take emission for current year or for the last emission
-        emissions[current_year] or emissions[emissions.keys.max]
-      end
-
-      # Determine in which scenario is current company emission
-      # @param company_emission [Float]
-      # @param scenarios [Hash]
-      def company_scenario(company_emission, scenarios)
-        scenarios_with_greater_emission = scenarios.select do |_s, value|
-          value >= company_emission
-        end
-
-        return scenarios.max.first if scenarios_with_greater_emission.empty?
-
-        scenarios_with_greater_emission.min_by { |_s, value| value - company_emission }.first
+        company_all_emissions[company_last_reported_year] || company_all_emissions[company_all_emissions.keys.max]
       end
 
       # @return [String] string with current year
