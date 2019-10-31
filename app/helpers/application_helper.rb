b9770953a650edead12459ad1b4504d4a6258676
Andrew Kuleshov <andrew.kuleshov@roonyx.team>
2019-10-31 14:53:41 +0300

    add styles for header and content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     return unless defined?(@breadcrumb)
 
     content_tag :div, class: 'breadcrumb has-succeeds-separator' do
-      content_tag :ui do
+      content_tag :ul do
         @breadcrumb.each_with_index.map do |item, i|
           content_tag :li, class: i.eql?(@breadcrumb.count - 1) ? 'is-active' : '' do
             link_to item.title, item.path
