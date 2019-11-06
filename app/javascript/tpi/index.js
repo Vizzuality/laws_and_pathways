c462d9e9af91103e41b6ec73c437d33f89310729
Tomasz Subik <tsubik@gmail.com>
2019-11-06 20:39:52 +0100

    remove not used code

--- app/javascript/tpi/index.js
+++ app/javascript/tpi/index.js
@@ -1,12 +1,1 @@
 import './controllers';
-import Turbolinks from 'turbolinks';
-
-Turbolinks.scroll = {};
-
-document.addEventListener("turbolinks:load", ()=> {
-  if (Turbolinks.scroll['cache']) {
-    document.scrollingElement.scrollTo(0, Turbolinks.scroll['cache']);
-  }
-
-  Turbolinks.scroll = {};
-});
