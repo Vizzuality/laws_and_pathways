d72fd71236108cf39c9700db99867ad019ae61d3
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-11-11 13:37:00 +0000

    Clean up

--- app/javascript/components/BubbleChart.js
+++ app/javascript/components/BubbleChart.js
@@ -1,12 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import { VictoryScatter, VictoryTooltip, VictoryGroup } from "victory";
 import SingleBubbleChart from './SingleBubbleChart';
 
 const COMPANIES_SIZES = {
   small: 18,
   medium: 33,
-  large: 48
+  large: 45
 };
 
 const LEVELS_COLORS = [
@@ -17,9 +16,6 @@ const LEVELS_COLORS = [
   '#083AAB',
   '#042b82'
 ];
-
-const width = 80;
-const height = 80;
 
 const BubbleChart = (data) => {
   const dataLevels = data.levels;
@@ -58,30 +54,19 @@ const BubbleChart = (data) => {
   );
 }
 
-// const VictoryBubbleChart = (bubbleColor, data) => (
-//   <VictoryGroup style={{ height: 'auto' }}>
-//     <VictoryScatter
-//       style={{
-//         data: { fill: bubbleColor, stroke: 'black', strokeWidth: '1px' }, labels: { fill: bubbleColor }
-//       }}
-//       minBubbleSize={COMPANIES_SIZES.small}
-//       maxBubbleSize={COMPANIES_SIZES.large}
-//       labels={({ datum }) => `${datum.name}\nLevel ${datum.level}\n${datum.companySize}`}
-//       labelComponent={<VictoryTooltip style={{ fontSize: '52px' }} />}
-//       data={data}
-//     />
-//   </VictoryGroup>
-// )
+const PackBubbleChart = (companiesBubbles, uniqueKey) => {
+  const width = 80;
+  const height = 38;
 
-const PackBubbleChart = (width, height, companiesBubbles, uniqueKey) => (
-  <SingleBubbleChart 
-    width={width}
-    height={height}
-    uniqueKey={uniqueKey}
-    data={companiesBubbles.length && companiesBubbles}
-    tooltipClassName='bubble-chart__tooltip'
-  />
-)
+  return (
+    <SingleBubbleChart 
+      width={width}
+      height={height}
+      uniqueKey={uniqueKey}
+      data={companiesBubbles.length && companiesBubbles}
+    />
+  )
+}
 
 const createRow = (dataRow, title) => { 
   return (
@@ -92,25 +77,15 @@ const createRow = (dataRow, title) => {
       {dataRow.map((el, i) => {
         const companiesBubbles = el.map(company => ({
           value: COMPANIES_SIZES[company.size],
-          tooltipContent: [company.name, `Level ${company.level}`, company.size],
+          tooltipContent: [company.name, `Level ${company.level}`],
           color: LEVELS_COLORS[i]
         }))
-
-        // const companiesBubblesVictory = el.map(company => ({
-        //   size: COMPANIES_SIZES[company.size],
-        //   companySize: company.size,
-        //   name: company.name,
-        //   level: company.level,
-        //   x: Math.random(),
-        //   y: Math.random()
-        // }))
 
         const uniqueKey = `${title}-${el.length}-${i}`;
 
         return (
           <div className="bubble-chart__cell" key={uniqueKey}>
-            {PackBubbleChart(width, height, companiesBubbles, uniqueKey)}
-            {/*VictoryBubbleChart(LEVELS_COLORS[i], companiesBubblesVictory)*/}
+            {PackBubbleChart(companiesBubbles, uniqueKey)}
           </div>
         )
       })}
--- app/javascript/components/SingleBubbleChart.js
+++ app/javascript/components/SingleBubbleChart.js
@@ -1,39 +1,21 @@
 import React, { Fragment, PureComponent } from 'react';
-import ReactTooltip from 'react-tooltip';
-import { pack, hierarchy } from 'd3-hierarchy';
-import { format } from 'd3-format';
 import { range } from 'd3-array';
 import { select } from 'd3-selection';
 import tip from 'd3-tip';
 import * as d3 from 'd3-force';
-import cx from 'classnames';
 
 class SingleBubbleChart extends PureComponent {
-  chartDataCalculation = (width, data) => {
-    const diameter = width - 10;
-    const bubble = pack().size([ diameter, diameter ]).padding(1.5);
-    const parsedData = { name: 'themes', children: data };
-    const root = hierarchy(parsedData)
-      .sum(d => d.value)
-      .sort((a, b) => b.value - a.value);
-    return bubble(root);
-  };
-
   render() {
     const {
       width,
       height,
       handleNodeClick,
       data,
-      tooltipClassName,
-      config,
       uniqueKey
     } = this.props;
-    const charData = data && this.chartDataCalculation(width, data);
 
     const computizedKey = uniqueKey.split(' ').join('_');
     const key = `${computizedKey.replace(/[&]/g, "_")}-${(Math.random()*100).toFixed()}`;
-
     const d3Tip = tip().attr('class', 'd3-tip').html(function(d) { return getTooltipText(d) })
     
     const nodes = range(data.length).map(function(index) {
@@ -68,64 +50,27 @@ class SingleBubbleChart extends PureComponent {
         .selectAll('circle')
         .data(nodes);
      
-      const v = u.enter()
+      const visualisation = u.enter()
         .append('circle')
-        .attr('r', function(d) {
-          return d.radius;
-        })
-        .style('fill', function(d) {
-          return d.color;
-        })
+        .attr('r', (d) => d.radius)
+        .style('fill', (d) => d.color)
         .merge(u)
-        .attr('cx', function(d) {
-          return d.x;
-        })
-        .attr('cy', function(d) {
-          return d.y;
-        })
-        .attr('data-for', function(d) {
-          return "chartTooltip"
-        })
+        .attr('cx', (d) => d.x)
+        .attr('cy', (d) => d.y)
         .on('mouseover', d3Tip.show)
         .on('mouseout', d3Tip.hide)
     
-      v.call(d3Tip);
+      visualisation.call(d3Tip); // bind the d3 tooltip with the visualisation
       u.exit().remove();
     }
 
     simulation();
 
     return (
-      <Fragment> {/* 40 -16 ${width} ${height} */}
-        <svg id={key} width={width} height={'38px'} viewBox="0 -100 400 400">
+      <Fragment>
+        <svg id={key} width={width} height={height} viewBox="0 -100 400 400">
           <g className='bubble-chart_circle' transform="translate(200, 100)"></g>
         </svg>
-        {/* <svg width={width} height='38px' viewBox={`0 0 ${width} ${height}`}>
-          {
-            charData && charData.children.map(d => (
-              <g
-                key={`${d.value}-${Math.random()}`}
-                // transform={`translate(${d.x * 2.5},${d.y / 1.8})`}
-                transform={`translate(${d.x},${d.y}) scale(1,-1)`}
-                className='bubble-chart_circle'
-                onClick={e => handleNodeClick(e, d.data.id)}
-              >
-                <circle
-                  r={charData.children.length > 2 ? d.r : d.value}
-                  data-for="chartTooltip"
-                  data-tip={this.getTooltipText(d.data, config)}
-                  fill={d.data.color}
-                />
-              </g>
-              ))
-          }
-        </svg>*/}
-        <ReactTooltip
-          place="left"
-          id="chartTooltip"
-          className={cx('reactTooltipWhite', tooltipClassName)}
-          multiline
-        />
       </Fragment>
     );
   }
