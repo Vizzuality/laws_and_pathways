d1e2b8bde228ad7555c47e5e891cb8350973d17b
Simao Belchior <simaobelchior@gmail.com>
2019-11-12 19:27:46 +0000

    Adds a table with the latest comments to the dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,9 +2,26 @@ ActiveAdmin.register_page 'Dashboard' do
   menu priority: 0, label: 'Dashboard'
 
   content do
-    div class: 'blank_slate_container' do
-      span class: 'blank_slate' do
-        span 'Laws and Pathways dashboard'
+    if ActiveAdmin::Comment.any?
+      h1 'Latest Comments'
+      div class: 'index_content' do
+        div class: 'index_as_table' do
+          table_for ActiveAdmin::Comment.order(created_at: :desc).limit(20),
+                    class: 'index_table index' do
+            column :resource
+            column :body do |comment|
+              simple_format comment.body
+            end
+            column :author
+            column :created_at
+          end
+        end
+      end
+    else
+      div class: 'blank_slate_container' do
+        span class: 'blank_slate' do
+          span 'No recent comments! All is good.'
+        end
       end
     end
   end
