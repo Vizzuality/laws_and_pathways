4749227f6aecc0d8dfffcee344d8c8134b763c18
Andrew Kuleshov <andrew.kuleshov@roonyx.team>
2019-11-21 16:39:29 +0300

    review fixes

--- app/services/api/latest_additions.rb
+++ app/services/api/latest_additions.rb
@@ -5,13 +5,13 @@ module Api
     end
 
     def call
-      (serialize_litigations + serialize_legislations).sort_by { |item| - item[:created_at].to_time.to_i }
+      (serialize_litigations + serialize_legislations).sort_by { |item| - (item[:date_passed] || 0) }
     end
 
     private
 
     def serialize_litigations
-      Litigation.last(@count).map do |item|
+      CCLOW::LitigationDecorator.decorate_collection(Litigation.published.last(@count)).map do |item|
         addition_type = if item.events.last&.event_type.present?
                           I18n.t("cclow.litigation.event_types.#{item.events.last&.event_type}")
                         end
@@ -20,20 +20,22 @@ module Api
          date_passed: item.started_event&.date&.year,
          iso: item.jurisdiction.iso,
          addition_type: addition_type,
-         jurisdiction: item.jurisdiction.name,
-         created_at: item.created_at}
+         jurisdiction: item.jurisdiction,
+         created_at: item.created_at,
+         link: item.link}
       end
     end
 
     def serialize_legislations
-      Legislation.last(@count).map do |item|
+      CCLOW::LegislationDecorator.decorate_collection(Legislation.published.last(@count)).map do |item|
         {kind: 'Laws and policies',
          title: item.title,
          date_passed: item.date_passed&.year,
          iso: item.geography.iso,
          addition_type: I18n.t("cclow.legislation_types.#{item.legislation_type}"),
-         jurisdiction: item.geography.name,
-         created_at: item.created_at}
+         jurisdiction: item.geography,
+         created_at: item.created_at,
+         link: item.link}
       end
     end
   end
