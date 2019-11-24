1ba6960292ac482dbebb39b60fbb7ac6bbc77f05
Marek Kowalcze <marek.kowalcze@gmail.com>
2019-11-24 16:12:48 +0100

    tweak display of activity index page

--- app/admin/activities.rb
+++ app/admin/activities.rb
@@ -1,15 +1,24 @@
 ActiveAdmin.register PublicActivity::Activity do
   config.per_page = 30
+  config.batch_actions = false
 
   menu parent: 'Administration', priority: 3, label: 'Users Activity'
 
   decorate_with PublicActivity::ActivityDecorator
 
-  index do
+  actions :all, except: [:new, :create]
+
+  filter :trackable_type, label: 'Record Type'
+  filter :trackable_id_equals, label: 'Record ID'
+  filter :owner_id_equals, label: 'User ID'
+  filter :key_contains, label: 'Action details contains'
+  filter :updated_at
+
+  index title: 'Users Activity' do
     column 'Record Type', :trackable_type
     column 'Record ID', :trackable_id
     column 'User', :owner
-    column 'Activity', &:activity_details
+    column 'Activity details', &:activity_details
     column 'Record', :trackable, &:trackable_link
     column :updated_at, &:updated_at_display
   end
--- app/decorators/public_activity/activity_decorator.rb
+++ app/decorators/public_activity/activity_decorator.rb
@@ -4,30 +4,37 @@ class PublicActivity::ActivityDecorator < Draper::Decorator
   TRACKABLE_DISPLAY_NAME_MAPPING = {
     'Litigation' => :title,
     'Legislation' => :title,
-    'Company' => :name
+    'Company' => :name,
+    'Geography' => :name
   }.freeze
 
   TRACKABLE_ACTION_KEYS_MAPPING = {
-    'draft' => 'moved to DRAFT state',
-    'pending' => 'moved to PENDING state',
-    'published' => 'moved to PUBLISHED state',
+    'draft' => 'moved to "Draft" state',
+    'pending' => 'moved to "Pending" state',
+    'published' => 'moved to "Published" state',
     'archived' => 'archived'
   }.freeze
 
   def activity_details
     resource, action = model.key.split('.')
+
     "#{resource} was #{TRACKABLE_ACTION_KEYS_MAPPING[action] || action}"
   end
 
   def updated_at_display
-    "#{h.time_ago_in_words(updated_at)} ago"
+    "#{h.time_ago_in_words(updated_at)} ago (#{updated_at})"
   end
 
   def trackable_link
+    # link to record if exists
     return h.link_to trackable_display_name(trackable) if trackable
 
+    # if trackable record does not exist, it could have been discarded
     discarded_trackable = find_discarded_trackable
-    trackable_display_name(discarded_trackable) if discarded_trackable
+    return trackable_display_name(discarded_trackable) if discarded_trackable
+
+    # fallback message
+    '(no record found)'
   end
 
   def find_discarded_trackable
@@ -35,6 +42,8 @@ class PublicActivity::ActivityDecorator < Draper::Decorator
   end
 
   def trackable_display_name(trackable_object)
-    trackable_object.send(TRACKABLE_DISPLAY_NAME_MAPPING[trackable_type] || :to_s).truncate(50)
+    display_method = TRACKABLE_DISPLAY_NAME_MAPPING[trackable_type] || :to_s
+
+    trackable_object.send(display_method).truncate(50)
   end
 end
