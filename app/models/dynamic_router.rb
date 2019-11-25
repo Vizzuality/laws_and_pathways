465c81b9dba39583294175ba98ac16deaed17cf4
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-11-25 13:44:00 +0000

    Fix the DynamicRouter routes name

--- app/models/dynamic_router.rb
+++ app/models/dynamic_router.rb
@@ -1,6 +1,6 @@
 class DynamicRouter < ApplicationRecord
   def self.load
-    Rails.application.routes.draw do
+    LawsAndPathways::Application.routes.draw do
       Page.all.each do |page|
         puts "Routing #{page.slug}"
         get "/tpi/#{page.slug}", :to => "tpi/pages#show", defaults: { id: page.id }
@@ -9,6 +9,6 @@ class DynamicRouter < ApplicationRecord
   end
 
   def self.reload
-    Rails.application.routes_reloader.reload!
+    LawsAndPathways::Application.routes_reloader.reload!
   end
 end
