044d56617a98d5c504ab2a25db07337d9670034f
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-11-25 15:31:06 +0000

    Try fixing travis - check if DB exists

--- app/services/dynamic_router.rb
+++ app/services/dynamic_router.rb
@@ -1,10 +1,17 @@
 class DynamicRouter
   class << self
     def load
-      LawsAndPathways::Application.routes.draw do
-        Page.all.each do |page|
-          puts "Routing #{page.slug}"
-          get "/tpi/#{page.slug}", :to => "tpi/pages#show", defaults: { id: page.id }
+      begin
+        # It will fail if the database does not exist
+        ActiveRecord::Base.connection
+      rescue
+        exit 1
+      else
+        LawsAndPathways::Application.routes.draw do
+          Page.all.each do |page|
+            puts "Routing #{page.slug}"
+            get "/tpi/#{page.slug}", :to => "tpi/pages#show", defaults: { id: page.id }
+          end
         end
       end
     end
