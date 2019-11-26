7106e609eb7185b0574da46462ee4a8f7302aa24
Andrew Kuleshov <andrew.kuleshov@roonyx.team>
2019-11-26 16:39:04 +0300

    remove extra template for events timeline

--- app/javascript/components/EventsTimeline.js
+++ app/javascript/components/EventsTimeline.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import Select, {components} from 'react-select';
 import PropTypes from "prop-types";
 import Testimonials from "./Testimonials";
+import moment from 'moment';
 
 Testimonials.propTypes = {
   events: PropTypes.array,
+  options: PropTypes.array,
 };
 
 const blueDarkColor = '#2E3152';
@@ -30,7 +32,7 @@ const customStyles = {
     borderRadius: '0',
     borderWidth: '4px',
     marginTop: '0',
-    width: '250px',
+    width: 250,
     right: '0',
     marginRight: '0',
     zIndex: 9,
@@ -43,43 +45,47 @@ const Option = props => (
       <input type="checkbox" hidden checked={props.isSelected} onChange={() => null} />
       {props.isSelected && <div className="checked select-checkbox"><i className="fa fa-check"></i></div>}
       {!props.isSelected && <div className="unchecked select-checkbox"></div>}
-      <label>{props.value}</label>
+      <label>{props.label}</label>
     </components.Option>
   </div>
 );
-const EventsTimeline = ({ events }) => {
-  const [currentTypes, setCurrentTypes] = useState(false);
-  let options = [...new Set(events.map((item)=>item.event_type))];
-  options = options.map((item) => ({value: item, label: item}));
-  if ((currentTypes || []).length !== 0) {
-    const types = currentTypes.map(e => e.value);
-    events = events.filter(e=> types.includes(e.event_type));
-  }
 
+const EventsTimeline = ({ events, options, isFiltered = false }) => {
+  const [currentTypes, setCurrentTypes] = useState(false);
   const el = React.createRef();
+    if (isFiltered && (currentTypes || []).length !== 0) {
+      const types = currentTypes.map(e => e.value);
+      events = events.filter(e => types.includes(e.event_type));
+    }
+
   return (
     <div className="timeline-events-container">
-      <div className="title-block">
+      <div className={isFiltered ? 'title-block' : ''}>
         <h5>Timeline of events</h5>
-        <div className="filter-block">
-          <span>Show events:</span>
-          <Select
-            components={{ Option }}
-            styles={customStyles}
-            options={options}
-            isMulti
-            placeholder='All events'
-            closeMenuOnSelect={false}
-            hideSelectedOptions={false}
-            onChange={(e) => setCurrentTypes(e)} />
-        </div>
+        {
+          isFiltered &&
+          <div className="filter-block">
+            <span>Show events:</span>
+            <Select
+              components={{ Option }}
+              styles={customStyles}
+              options={options}
+              isMulti
+              placeholder='All events'
+              closeMenuOnSelect={false}
+              hideSelectedOptions={false}
+              onChange={(e) => setCurrentTypes(e)} />
+          </div>
+        }
       </div>
-      <div className="topic-details">
-        Use the dropdown menu to select the different events you wish to see displayed on the country timeline.
-      </div>
-
+      {
+        isFiltered &&
+          <div className="topic-details">
+            Use the dropdown menu to select the different events you wish to see displayed on the country timeline.
+          </div>
+      }
       <div className="timeline-events">
-        <div className="arrow-left" onClick={() => {el.current.scrollBy(-40,0)}}>
+        <div className="arrow-left" onClick={() => {el.current.scrollBy(-40, 0)}}>
           <i className="fa fa-arrow-left"></i>
         </div>
         <div ref={ el } className="events-container">
@@ -88,12 +94,12 @@ const EventsTimeline = ({ events }) => {
               <div key={i} className="time-point">
                 <div className="event-title">{ event.title }</div>
                 <div className="point"></div>
-                <div className="date">{ event.date }</div>
+                <div className="date">{ moment(event.date).format('MMMM Y') }</div>
               </div>
             ))}
           </div>
         </div>
-        <div className="arrow-right" onClick={() => el.current.scrollBy(40,0)}>
+        <div className="arrow-right" onClick={() => el.current.scrollBy(40, 0)}>
           <i className="fa fa-arrow-right"></i>
         </div>
       </div>
