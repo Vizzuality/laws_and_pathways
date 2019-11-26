4564e607d23395e982963ced2fd0b8e9459c1f29
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-11-26 15:52:10 +0000

    Add dependent on images to Content model

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,6 @@
 class Content < ApplicationRecord
   belongs_to :page
-  has_many :images
+  has_many :images, dependent: :destroy
 
   with_options allow_destroy: true, reject_if: :all_blank do
     accepts_nested_attributes_for :images
