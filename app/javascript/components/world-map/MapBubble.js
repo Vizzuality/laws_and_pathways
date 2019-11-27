a79328d12e6cce418207337c3c3d5c99c7447da5
sorodrigo <rodrigo.solis@vizzuality.com>
2019-11-26 20:36:25 -0600

    includes world-map component with fake layers and bubbles

---
+++ app/javascript/components/world-map/MapBubble.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Marker } from 'react-simple-maps';
+
+function MapBubble(props) {
+  const { weight, color, opacity, ...markerProps } = props;
+  return (
+    <Marker {...markerProps} onMouseDown={() => console.log(props)}>
+      <circle
+        r={weight}
+        fill={color}
+        fillOpacity={opacity}
+        className="world-map__map-bubble"
+      />
+    </Marker>
+  );
+}
+
+export default MapBubble;
