7dac5cee790287356541e5ccf8367802015ace52
Tomasz Subik <tsubik@gmail.com>
2019-11-27 11:03:22 +0100

    fixing loading dynamic routes when no database

--- app/services/dynamic_router.rb
+++ app/services/dynamic_router.rb
@@ -1,17 +1,14 @@
 class DynamicRouter
   class << self
     def load
-      unless ActiveRecord::Base.connection.table_exists?(:pages) ||
-          (!Rails.env.development? && ActiveRecord::Base.connected?)
-        return
-      end
-
       LawsAndPathways::Application.routes.draw do
         Page.all.each do |page|
-          puts "Routing #{page.slug}"
           get "/tpi/#{page.slug}", to: 'tpi/pages#show', defaults: {id: page.id}
         end
       end
+    rescue PG::ConnectionBad, ActiveRecord::NoDatabaseError
+      puts $!
+      puts 'No database or connection failed. Skipping DynamicRoutes load'
     end
 
     def reload
