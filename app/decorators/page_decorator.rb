8a6379b2d8512226d032bfd057892b842d4f1931
Tomasz Subik <tsubik@gmail.com>
2019-11-28 21:39:57 +0100

    add ability to set newRecordId for nested-list-controller

--- app/decorators/page_decorator.rb
+++ app/decorators/page_decorator.rb
@@ -1,6 +1,10 @@
 class PageDecorator < Draper::Decorator
   delegate_all
 
+  def title_link
+    h.link_to model.title, h.admin_page_path(model)
+  end
+
   def description
     model.description.html_safe
   end
