f16d6bcfb23c77b1af853a8f21a4edf83358c9ba
Tomasz Subik <tsubik@gmail.com>
2019-11-29 15:17:15 +0100

    fix cp unit seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,8 +24,9 @@ end
   ['Services'],
   ['Steel', 'Carbon intensity (tonnes of CO2 per tonne of steel)']
 ].each do |sector_name, sector_cp_unit|
-  sector = TPISector.find_or_create_by!(name: sector_name)
-  sector.update!(cp_unit: sector_cp_unit) if sector.new_record? && sector_cp_unit.present?
+  TPISector.find_or_create_by!(name: sector_name) do |sector|
+    sector.cp_unit = sector_cp_unit
+  end
 end
 
 # instruments: Instrument Type & Instrument
