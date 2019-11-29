f8829c6187e250e5d26f6ec2fca475fec0ab0c9d
Simao Belchior <simaobelchior@gmail.com>
2019-11-29 08:17:20 +0000

    fix generate file quotes

--- spec/factories/news_articles.rb
+++ spec/factories/news_articles.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :news_article do
-    title { "MyString" }
-    content { "MyText" }
-    publication_date { "2019-11-29" }
+    title { 'MyString' }
+    content { 'MyText' }
+    publication_date { '2019-11-29' }
 
     association :created_by, factory: :admin_user
     association :updated_by, factory: :admin_user
