b70c75cf59fc0c90900a3d964aa790d9e0b015b2
Marek Kowalcze <marek.kowalcze@gmail.com>
2019-12-02 12:38:50 +0100

    revert changes in app-controller for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,4 @@ class ApplicationController < ActionController::Base
   def set_current_user
     ::Current.admin_user = current_admin_user
   end
-
-  rescue_from CanCan::AccessDenied do |exception|
-    respond_to do |format|
-      format.json { head :forbidden, content_type: 'text/html' }
-      format.html { redirect_to main_app.root_url, notice: exception.message }
-      format.js   { head :forbidden, content_type: 'text/html' }
-    end
-  end
 end
