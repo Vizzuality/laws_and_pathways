60f6d90ea4ef0b6d72f982abcf08f649f21e27bf
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-12-03 09:25:00 +0000

    Hook up publications in the FE

--- app/controllers/tpi/publications_controller.rb
+++ app/controllers/tpi/publications_controller.rb
@@ -1,5 +1,15 @@
 module TPI
   class PublicationsController < TPIController
-    def index; end
+    def index 
+      publications = Publication.all.sort_by(&:created_at)
+      news = NewsArticle.all.sort_by(&:created_at)
+
+      @publications_and_articles = publications + news
+    end
+
+    def show
+      @publication = params[:type].eql?("NewsArticle") ? NewsArticle.find(params[:id]) : Publication.find(params[:id])
+      redirect_to '' unless @publication
+    end
   end
 end
