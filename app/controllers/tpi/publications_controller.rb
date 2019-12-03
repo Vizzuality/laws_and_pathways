4d53e0afb87807dd44b37e9072a47c8b6f43ae6a
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-12-03 09:31:48 +0000

    Rubocop

--- app/controllers/tpi/publications_controller.rb
+++ app/controllers/tpi/publications_controller.rb
@@ -1,6 +1,6 @@
 module TPI
   class PublicationsController < TPIController
-    def index 
+    def index
       publications = Publication.all.sort_by(&:created_at)
       news = NewsArticle.all.sort_by(&:created_at)
 
@@ -8,7 +8,7 @@ module TPI
     end
 
     def show
-      @publication = params[:type].eql?("NewsArticle") ? NewsArticle.find(params[:id]) : Publication.find(params[:id])
+      @publication = params[:type].eql?('NewsArticle') ? NewsArticle.find(params[:id]) : Publication.find(params[:id])
       redirect_to '' unless @publication
     end
   end
