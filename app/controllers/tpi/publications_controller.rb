184d0e8e8be5fefff1ba21e27aab34207157b1cf
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-12-04 17:53:31 +0000

    Move filtering to helper function

--- app/controllers/tpi/publications_controller.rb
+++ app/controllers/tpi/publications_controller.rb
@@ -1,48 +1,61 @@
 module TPI
   class PublicationsController < TPIController
+    # rubocop:disable Metrics/AbcSize
     def index
-      # tags assigned to at least one Publication/NewsArticle
-      @tags = (NewsArticle.all.map(&:keywords_list).flatten + Publication.all.map(&:keywords_list).flatten).uniq
-      # sectors assigned to at least one Publication/NewsArticle
-      @sectors = (Publication.joins(:sector).select('tpi_sectors.name as sector_name').map(&:sector_name) + 
+      @tags = (
+        NewsArticle.all.map(&:keywords_list).flatten +
+        Publication.all.map(&:keywords_list).flatten
+      ).uniq
+      @sectors = (Publication.joins(:sector).select('tpi_sectors.name as sector_name').map(&:sector_name) +
         NewsArticle.joins(:sector).select('tpi_sectors.name as sector_name').map(&:sector_name)).uniq
 
-      if params[:activeTags].present?
-        @active_tags = params[:activeTags].tr('[]', '').split(', ')
-        publications_by_tags = Publication.joins(:keywords).where(:tags => { :name => @active_tags }).order(publication_date: :desc)
-        news_by_tags = NewsArticle.joins(:keywords).where(:tags => { :name => @active_tags }).order(publication_date: :desc)
-      else
-        publications_by_tags = Publication.order(publication_date: :desc)
-        news_by_tags = NewsArticle.order(publication_date: :desc)
-        @active_tags = []
-      end
+      filter_by_tags
+      filter_by_sectors
 
-      if params[:activeSectors].present?
-        @active_sectors = params[:activeSectors].tr('[]', '').split(', ')
-        publications_by_sectors = Publication.joins(:sector).where('tpi_sectors.name IN (?)', @active_sectors)
-        news_by_sectors = NewsArticle.joins(:sector).where('tpi_sectors.name IN (?)', @active_sectors)
-      else
-        publications_by_sectors = Publication.order(publication_date: :desc)
-        news_by_sectors = NewsArticle.order(publication_date: :desc)
-        @active_sectors = []
-      end
+      publications = (@publications_by_tags + @publications_by_sectors).uniq
+      news = (@news_by_tags + @news_by_sectors).uniq
 
-      publications = (publications_by_tags + publications_by_sectors).uniq
-      news = (news_by_tags + news_by_sectors).uniq
-
-      if params[:filtersOpen].present?
-        @filters_open = params[:filtersOpen]
-      else
-        @filters_open = false
-      end
+      @filters_open = if params[:filtersOpen].present?
+                        params[:filtersOpen]
+                      else
+                        false
+                      end
 
       @publications_and_articles = (publications.uniq + news.uniq)
         .sort { |a, b| b.publication_date <=> a.publication_date }
     end
+    # rubocop:enable Metrics/AbcSize
 
     def show
       @publication = params[:type].eql?('NewsArticle') ? NewsArticle.find(params[:id]) : Publication.find(params[:id])
       redirect_to '' unless @publication
+    end
+
+    private
+
+    def filter_by_tags
+      if params[:activeTags].present?
+        @active_tags = params[:activeTags].tr('[]', '').split(', ')
+        @publications_by_tags = Publication.joins(:keywords)
+          .where(tags: {name: @active_tags}).order(publication_date: :desc)
+        @news_by_tags = NewsArticle.joins(:keywords).where(tags: {name: @active_tags}).order(publication_date: :desc)
+      else
+        @publications_by_tags = Publication.order(publication_date: :desc)
+        @news_by_tags = NewsArticle.order(publication_date: :desc)
+        @active_tags = []
+      end
+    end
+
+    def filter_by_sectors
+      if params[:activeSectors].present?
+        @active_sectors = params[:activeSectors].tr('[]', '').split(', ')
+        @publications_by_sectors = Publication.joins(:sector).where('tpi_sectors.name IN (?)', @active_sectors)
+        @news_by_sectors = NewsArticle.joins(:sector).where('tpi_sectors.name IN (?)', @active_sectors)
+      else
+        @publications_by_sectors = Publication.order(publication_date: :desc)
+        @news_by_sectors = NewsArticle.order(publication_date: :desc)
+        @active_sectors = []
+      end
     end
   end
 end
