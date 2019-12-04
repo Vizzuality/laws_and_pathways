1981d396aa5fdddbe9462fa69c1067d7d37e79f1
Simao Belchior <simaobelchior@gmail.com>
2019-12-04 00:09:58 +0000

    Sort properly, link well

--- app/controllers/tpi/publications_controller.rb
+++ app/controllers/tpi/publications_controller.rb
@@ -5,7 +5,7 @@ module TPI
       news = NewsArticle.order(:created_at)
 
       @publications_and_articles = (publications + news)
-        .sort{|a,b| b.publication_date <=> a.publication_date}
+        .sort { |a, b| b.publication_date <=> a.publication_date }
     end
 
     def show
