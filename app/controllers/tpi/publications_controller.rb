6aff0ff7e8f0f032bd5f95f78831fa2620289047
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-12-04 10:42:26 +0000

    Add react component scaffold for Publication filters

--- app/controllers/tpi/publications_controller.rb
+++ app/controllers/tpi/publications_controller.rb
@@ -4,6 +4,10 @@ module TPI
       publications = Publication.order(publication_date: :desc)
       news = NewsArticle.order(publication_date: :desc)
 
+      publication_titles = Publication.pluck(:title)
+      news_articles_titles = NewsArticle.pluck(:title)
+      @all_titles = (publication_titles + news_articles_titles).uniq
+
       @publications_and_articles = (publications + news)
         .sort { |a, b| b.publication_date <=> a.publication_date }
     end
