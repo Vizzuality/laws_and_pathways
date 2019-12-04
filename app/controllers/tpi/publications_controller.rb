d97e5bed3397e73117dd55ee33ad6f5d6c5b3e2e
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-12-04 17:33:33 +0000

    Add filtering for publications and news articles

--- app/controllers/tpi/publications_controller.rb
+++ app/controllers/tpi/publications_controller.rb
@@ -1,14 +1,42 @@
 module TPI
   class PublicationsController < TPIController
     def index
-      publications = Publication.order(publication_date: :desc)
-      news = NewsArticle.order(publication_date: :desc)
+      # tags assigned to at least one Publication/NewsArticle
+      @tags = (NewsArticle.all.map(&:keywords_list).flatten + Publication.all.map(&:keywords_list).flatten).uniq
+      # sectors assigned to at least one Publication/NewsArticle
+      @sectors = (Publication.joins(:sector).select('tpi_sectors.name as sector_name').map(&:sector_name) + 
+        NewsArticle.joins(:sector).select('tpi_sectors.name as sector_name').map(&:sector_name)).uniq
 
-      publication_titles = Publication.pluck(:title)
-      news_articles_titles = NewsArticle.pluck(:title)
-      @all_titles = (publication_titles + news_articles_titles).uniq
+      if params[:activeTags].present?
+        @active_tags = params[:activeTags].tr('[]', '').split(', ')
+        publications_by_tags = Publication.joins(:keywords).where(:tags => { :name => @active_tags }).order(publication_date: :desc)
+        news_by_tags = NewsArticle.joins(:keywords).where(:tags => { :name => @active_tags }).order(publication_date: :desc)
+      else
+        publications_by_tags = Publication.order(publication_date: :desc)
+        news_by_tags = NewsArticle.order(publication_date: :desc)
+        @active_tags = []
+      end
 
-      @publications_and_articles = (publications + news)
+      if params[:activeSectors].present?
+        @active_sectors = params[:activeSectors].tr('[]', '').split(', ')
+        publications_by_sectors = Publication.joins(:sector).where('tpi_sectors.name IN (?)', @active_sectors)
+        news_by_sectors = NewsArticle.joins(:sector).where('tpi_sectors.name IN (?)', @active_sectors)
+      else
+        publications_by_sectors = Publication.order(publication_date: :desc)
+        news_by_sectors = NewsArticle.order(publication_date: :desc)
+        @active_sectors = []
+      end
+
+      publications = (publications_by_tags + publications_by_sectors).uniq
+      news = (news_by_tags + news_by_sectors).uniq
+
+      if params[:filtersOpen].present?
+        @filters_open = params[:filtersOpen]
+      else
+        @filters_open = false
+      end
+
+      @publications_and_articles = (publications.uniq + news.uniq)
         .sort { |a, b| b.publication_date <=> a.publication_date }
     end
 
