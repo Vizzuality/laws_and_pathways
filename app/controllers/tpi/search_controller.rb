5bc39e660b31588d81fe60d519409fbc4e7435a7
Tomasz Subik <tsubik@gmail.com>
2019-12-04 17:33:19 +0100

    sort by titles

--- app/controllers/tpi/search_controller.rb
+++ app/controllers/tpi/search_controller.rb
@@ -3,12 +3,10 @@ module TPI
     def index
       @sectors = TPISector.search(params[:query]).order(:name)
       @companies = Company.search(params[:query]).order(:name)
-      
+
       publications = Publication.search(params[:query]).order(:title)
       news = NewsArticle.search(params[:query]).order(:title)
-      @publications_and_articles = (publications + news).sort { |a, b|
-        a.title <=> b.title
-      }
+      @publications_and_articles = (publications + news).sort_by(:title)
 
       @query = params[:query]
     end
