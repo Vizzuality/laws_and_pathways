bc245b6613c24dc496f0b45f91c815c662e1b2b2
Tomasz Subik <tsubik@gmail.com>
2019-12-05 22:30:57 +0100

    fix slick carousel

--- app/javascript/packs/cclow.js
+++ app/javascript/packs/cclow.js
@@ -1,5 +1,5 @@
 /* import Turbolinks from "turbolinks"; */
-/* import Slick from "slick-carousel"; */
+import Slick from 'slick-carousel';
 import Chartkick from 'chartkick';
 import HighCharts from 'highcharts';
 import Rails from '@rails/ujs';
@@ -10,6 +10,7 @@ import 'shared';
 
 Chartkick.use(HighCharts);
 
+window.Slick = Slick;
 window.Rails = Rails;
 window.$ = $;
 
