9ce12424c999159b61e5ae0e4e5390da83d03532
Simao Belchior <simaobelchior@gmail.com>
2019-12-06 18:32:47 +0000

    enable rolling back migrations

--- db/migrate/20191206164849_create_join_table_legislation_laws_sectors.rb
+++ db/migrate/20191206164849_create_join_table_legislation_laws_sectors.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableLegislationLawsSectors < ActiveRecord::Migration[6.0]
   def change
-    remove_column :legislations, :sector_id
+    remove_column :legislations, :sector_id, :bigint
     create_join_table :legislations, :laws_sectors do |t|
       t.index :legislation_id
       t.index :laws_sector_id
--- db/migrate/20191206175055_create_join_table_litigations_laws_sectors.rb
+++ db/migrate/20191206175055_create_join_table_litigations_laws_sectors.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableLitigationsLawsSectors < ActiveRecord::Migration[6.0]
   def change
-    remove_column :litigations, :sector_id
+    remove_column :litigations, :sector_id, :bigint
     create_join_table :litigations, :laws_sectors do |t|
       t.index :litigation_id
       t.index :laws_sector_id
