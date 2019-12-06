15077cbb026f639962090557a6779085dceb20e8
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-12-06 18:13:45 +0000

    Add tooltip for the CCLOW map * Tooltip appears on bubble click * Set stroke for clicked circle * Tooltip is clickable - set a link inside * Style the tooltip

--- app/javascript/components/world-map/MapBubble.js
+++ app/javascript/components/world-map/MapBubble.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Marker } from 'react-simple-maps';
 
 function MapBubble(props) {
   const { weight, color, opacity, ...markerProps } = props;
+  const [clicked, setClicked] = useState(false);
+
+  const bubble = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (bubble.current && !bubble.current.contains(event.target)) {
+      setClicked(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <Marker {...markerProps} onMouseDown={() => {}}>
+    <Marker {...markerProps} onMouseDown={() => setClicked(!clicked)}>
       <circle
+        ref={bubble}
         r={weight}
         fill={color}
         fillOpacity={opacity}
+        strokeWidth={clicked ? '2' : '0'}
+        stroke="#2E3152"
         className="world-map__map-bubble"
       />
     </Marker>
