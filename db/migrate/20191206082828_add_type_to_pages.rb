549c6f87f135f8d3e2fd6f4ccac27d476e6809bf
Simao Belchior <simaobelchior@gmail.com>
2019-12-06 10:09:35 +0000

    Initialize Page

--- db/migrate/20191206082828_add_type_to_pages.rb
+++ db/migrate/20191206082828_add_type_to_pages.rb
@@ -1,4 +1,5 @@
 class AddTypeToPages < ActiveRecord::Migration[6.0]
+  Page = Class.new(ActiveRecord::Base)
   def up
     add_column :pages, :type, :string
     Page.connection.execute("UPDATE pages SET type='TPIPage'")
