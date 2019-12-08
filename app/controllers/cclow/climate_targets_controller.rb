e8ebe4e1a4828afd756f9d274026de8c35ac04af
Simao Belchior <simaobelchior@gmail.com>
2019-12-08 20:53:40 +0000

    fixing targets index page

--- app/controllers/cclow/climate_targets_controller.rb
+++ app/controllers/cclow/climate_targets_controller.rb
@@ -3,9 +3,11 @@ module CCLOW
     include SearchController
 
     def index
+      add_breadcrumb('Climate Targets', cclow_climate_targets_path)
       if params[:ids]
         ids = params[:ids].split(',').map(&:to_i)
         @climate_targets = Target.find(ids)
+        add_breadcrumb('Search results', request.path)
       else
         @climate_targets = Target.all
       end
