f1d01d3c50b1c8c2be0822e6e97bb576c00894ce
Simao Belchior <simaobelchior@gmail.com>
2019-12-08 19:51:03 +0000

    Adds eu_member? method, and some more front end tweaks

--- app/models/geography.rb
+++ app/models/geography.rb
@@ -54,6 +54,11 @@ class Geography < ApplicationRecord
     'Latin America & Caribbean'
   ].freeze
 
+  EU_COUNTRIES = %w[
+    AUT BEL BGR CYP CZE DEU DNK ESP EST FIN FRA GBR GRC HRV HUN IRL ITA LTU
+    LUX LVA MLT NLD POL PRT ROU SVK SVN SWE
+  ].freeze
+
   enum geography_type: array_to_enum_hash(GEOGRAPHY_TYPES)
 
   tag_with :political_groups
@@ -75,5 +80,9 @@ class Geography < ApplicationRecord
     return unless national?
 
     "https://www4.unfccc.int/sites/NDCStaging/pages/Party.aspx?party=#{iso}"
+  end
+
+  def eu_member?
+    EU_COUNTRIES.include?(iso)
   end
 end
