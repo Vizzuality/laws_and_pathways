05388f46c9e4f85403b341dd0b14b336436048e2
Simao Belchior <simaobelchior@gmail.com>
2019-12-09 01:18:20 +0000

    fix typo

--- app/controllers/cclow/legislation_and_policies_controller.rb
+++ app/controllers/cclow/legislation_and_policies_controller.rb
@@ -6,10 +6,10 @@ module CCLOW
       add_breadcrumb('Legislation and policies', cclow_legislation_and_policies_path(@geography))
       if params[:fromDate]
         @legislations = CCLOW::LegislationDecorator
-          .decorate_collection(Legislation.plubished.where('updated_at >= ?', params[:fromDate]))
+          .decorate_collection(Legislation.published.where('updated_at >= ?', params[:fromDate]))
       elsif params[:ids]
         ids = params[:ids].split(',').map(&:to_i)
-        @legislations = CCLOW::LegislationDecorator.decorate_collection(Legislation.plubished.find(ids))
+        @legislations = CCLOW::LegislationDecorator.decorate_collection(Legislation.published.find(ids))
         add_breadcrumb('Search results', request.path)
       else
         @legislations = CCLOW::LegislationDecorator.decorate_collection(Legislation.published)
