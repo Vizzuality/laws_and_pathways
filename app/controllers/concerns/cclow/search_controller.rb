a8aaf39dbbd6bd8a876f344e68f9be2fe104c2d7
Simao Belchior <simaobelchior@gmail.com>
2019-12-09 11:34:57 +0000

    Filter only published

--- app/controllers/concerns/cclow/search_controller.rb
+++ app/controllers/concerns/cclow/search_controller.rb
@@ -6,6 +6,7 @@ module CCLOW
       before_action :set_search_attributes
     end
 
+    # rubocop:disable Metrics/AbcSize
     def set_search_attributes
       geography_columns = ::Geography.attribute_names -
         %w[created_at updated_at created_by_id updated_by_id discarded_at]
@@ -15,6 +16,7 @@ module CCLOW
         %w[id law_id geography_id slug created_at updated_at
            created_by_id updated_by_id discarded_at sector_id parent_id]
       @search_laws_and_policies = Legislation.joins(:geography)
+        .where(legislations: {visibility_status: 'published'})
         .select(:id, laws_columns, 'geographies.name as geography_name')
       litigation_columns = Litigation.attribute_names -
         %w[id slug citation_reference_number jurisdiction_id at_issue
@@ -24,9 +26,11 @@ module CCLOW
         .where(litigations: {visibility_status: 'published'})
       target_columns = Target.attribute_names -
         %w[id geography_id target_scope_id created_at updated_at created_by_id updated_by_id discarded_at sector_id]
-      @search_targets = Target.joins(:geography).select(:id, target_columns, 'geographies.name as geography_name')
+      @search_targets = Target.published
+        .joins(:geography).select(:id, target_columns, 'geographies.name as geography_name')
       @search_recent_date = 1.year.ago.strftime('%F')
       @query = params[:q]
     end
+    # rubocop:enable Metrics/AbcSize
   end
 end
