0eee5086ff79b5e03cbee6ca366e50b74d92a3fa
Andrew Kuleshov <andrew.kuleshov@roonyx.team>
2019-12-09 12:16:05 +0300

    pass legislation_and_policies page to react component

--- app/controllers/cclow/legislation_and_policies_controller.rb
+++ app/controllers/cclow/legislation_and_policies_controller.rb
@@ -2,6 +2,7 @@ module CCLOW
   class LegislationAndPoliciesController < CCLOWController
     include SearchController
 
+    # rubocop:disable Metrics/AbcSize
     def index
       add_breadcrumb('Legislation and policies', cclow_legislation_and_policies_path(@geography))
       if params[:fromDate]
@@ -14,6 +15,32 @@ module CCLOW
       else
         @legislations = CCLOW::LegislationDecorator.decorate_collection(Legislation.published)
       end
+      filter
+      geography_options = {field_name: 'geography', options: ::Geography.all.map { |l| {value: l.id, label: l.name} }}
+      region_options = {field_name: 'region', options: ::Geography::REGIONS.map { |l| {value: l, label: l} }}
+
+      respond_to do |format|
+        format.html do
+          render component: 'pages/cclow/LegislationAndPolicies', props: {
+            filter_option: [region_options, geography_options],
+            legislations: @legislations.first(10),
+            count: @legislations.count
+          }, prerender: false
+        end
+        format.json { render json: {legislations: @legislations.last(10), count: @legislations.count} }
+      end
+    end
+    # rubocop:enable Metrics/AbcSize
+
+    private
+
+    def filter
+      if params[:region]
+        @legislations = @legislations.includes(:geography).where(geographies: {region: params[:region]})
+      end
+      return unless params[:geography]
+
+      @legislations = @legislations.includes(:geography).where(geographies: {id: params[:geography]})
     end
   end
 end
