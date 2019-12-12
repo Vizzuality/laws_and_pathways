5d8cb570c0b3103f61d48312711df381add24af6
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-12-12 10:53:24 +0000

    Add missing spacer on mobile homepage + responsive slider

--- app/javascript/shared/controllers/slider_controller.js
+++ app/javascript/shared/controllers/slider_controller.js
@@ -4,7 +4,17 @@ export default class extends Controller {
   connect() {
     $(this.element).slick({
       prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-1x fa-arrow-left"></i></button>',
-      nextArrow: '<button type="button" class="slick-next"><i class="fa fa-1x fa-arrow-right"></i></button>'
+      nextArrow: '<button type="button" class="slick-next"><i class="fa fa-1x fa-arrow-right"></i></button>',
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false
+          }
+        }
+      ]
     });
   }
 
