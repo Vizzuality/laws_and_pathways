3d584f9a3d9a3445ba21c7d0b86ad45607ebb22f
Simao Belchior <simaobelchior@gmail.com>
2019-12-16 18:14:23 +0000

    add params[:q] to permitted params

--- app/controllers/concerns/cclow/filter_controller.rb
+++ app/controllers/concerns/cclow/filter_controller.rb
@@ -14,7 +14,7 @@ module CCLOW
     end
 
     def filter_params
-      params.permit(:fromDate, :recent, :ids, region: [], geography: [], tags: [])
+      params.permit(:q, :fromDate, :recent, :ids, region: [], geography: [], tags: [])
     end
   end
 end
