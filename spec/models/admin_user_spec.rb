4776ed397ebdbc64fb1741ff16315eaa1fde2fa2
Marek Kowalcze <marek.kowalcze@gmail.com>
2020-01-04 17:53:45 +0100

    unit tests for publisher roles

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -41,34 +41,48 @@ RSpec.describe AdminUser, type: :model do
       let(:admin_user) { build(:admin_user, role: 'editor_tpi') }
 
       context 'when creating' do
-        Ability::TPI_RESOURCES.each do |resource|
-          it { is_expected.to be_able_to(:create, resource) }
+        context 'can' do
+          Ability::TPI_RESOURCES.each do |resource|
+            it { is_expected.to be_able_to(:create, resource) }
+          end
         end
 
-        Ability::LAWS_RESOURCES.each do |resource|
-          it { is_expected.not_to be_able_to(:create, resource) }
+        context 'can not' do
+          Ability::LAWS_RESOURCES.each do |resource|
+            it { is_expected.not_to be_able_to(:create, resource) }
+          end
+
+          it { is_expected.not_to be_able_to(:create, AdminUser) }
         end
       end
 
       context 'when updating' do
-        Ability::TPI_RESOURCES.each do |resource|
-          it { is_expected.to be_able_to(:update, resource) }
+        context 'can' do
+          Ability::TPI_RESOURCES.each do |resource|
+            it { is_expected.to be_able_to(:update, resource) }
+          end
         end
 
-        Ability::LAWS_RESOURCES.each do |resource|
-          it { is_expected.not_to be_able_to(:update, resource) }
+        context 'can not' do
+          Ability::LAWS_RESOURCES.each do |resource|
+            it { is_expected.not_to be_able_to(:update, resource) }
+          end
         end
       end
 
       context 'when archiving' do
-        it { is_expected.to be_able_to(:archive, Company) }
-
-        Ability::LAWS_RESOURCES.each do |resource|
-          it { is_expected.not_to be_able_to(:archive, resource) }
+        context 'can' do
+          it { is_expected.to be_able_to(:archive, Company) }
         end
 
-        (Ability::TPI_RESOURCES - Ability::PUBLISHABLE_RESOURCES).each do |resource|
-          it { is_expected.not_to be_able_to(:archive, resource) }
+        context 'can not' do
+          Ability::LAWS_RESOURCES.each do |resource|
+            it { is_expected.not_to be_able_to(:archive, resource) }
+          end
+
+          (Ability::TPI_RESOURCES - Ability::PUBLISHABLE_RESOURCES).each do |resource|
+            it { is_expected.not_to be_able_to(:archive, resource) }
+          end
         end
       end
     end
@@ -77,36 +91,128 @@ RSpec.describe AdminUser, type: :model do
       let(:admin_user) { build(:admin_user, role: 'editor_laws') }
 
       context 'when creating' do
-        Ability::LAWS_RESOURCES.each do |resource|
-          it { is_expected.to be_able_to(:create, resource) }
+        context 'can' do
+          Ability::LAWS_RESOURCES.each do |resource|
+            it { is_expected.to be_able_to(:create, resource) }
+          end
         end
 
-        Ability::TPI_RESOURCES.each do |resource|
-          it { is_expected.not_to be_able_to(:create, resource) }
+        context 'can not' do
+          Ability::TPI_RESOURCES.each do |resource|
+            it { is_expected.not_to be_able_to(:create, resource) }
+          end
+
+          it { is_expected.not_to be_able_to(:create, AdminUser) }
         end
       end
 
       context 'when updating' do
-        Ability::LAWS_RESOURCES.each do |resource|
-          it { is_expected.to be_able_to(:update, resource) }
+        context 'can' do
+          Ability::LAWS_RESOURCES.each do |resource|
+            it { is_expected.to be_able_to(:update, resource) }
+          end
         end
 
-        Ability::TPI_RESOURCES.each do |resource|
-          it { is_expected.not_to be_able_to(:update, resource) }
+        context 'can not' do
+          Ability::TPI_RESOURCES.each do |resource|
+            it { is_expected.not_to be_able_to(:update, resource) }
+          end
         end
       end
 
       context 'when archiving' do
-        it { is_expected.to be_able_to(:archive, Legislation) }
-        it { is_expected.to be_able_to(:archive, Litigation) }
-        it { is_expected.to be_able_to(:archive, Target) }
-
-        Ability::TPI_RESOURCES.each do |resource|
-          it { is_expected.not_to be_able_to(:archive, resource) }
+        context 'can' do
+          it { is_expected.to be_able_to(:archive, Legislation) }
+          it { is_expected.to be_able_to(:archive, Litigation) }
+          it { is_expected.to be_able_to(:archive, Target) }
         end
 
-        (Ability::LAWS_RESOURCES - Ability::PUBLISHABLE_RESOURCES).each do |resource|
-          it { is_expected.not_to be_able_to(:archive, resource) }
+        context 'can not' do
+          Ability::TPI_RESOURCES.each do |resource|
+            it { is_expected.not_to be_able_to(:archive, resource) }
+          end
+
+          (Ability::LAWS_RESOURCES - Ability::PUBLISHABLE_RESOURCES).each do |resource|
+            it { is_expected.not_to be_able_to(:archive, resource) }
+          end
+        end
+      end
+    end
+
+    context 'when user is TPI publisher' do
+      let(:admin_user) { build(:admin_user, role: 'publisher_tpi') }
+
+      context 'when managing' do
+        context 'can' do
+          Ability::TPI_RESOURCES.each do |resource|
+            it { is_expected.to be_able_to(:crud, resource) }
+          end
+
+          it { is_expected.to be_able_to(:crud, Tag) }
+          it { is_expected.to be_able_to(:create, AdminUser) }
+        end
+      end
+
+      context 'when archiving / publishing' do
+        context 'can' do
+          it { is_expected.to be_able_to(:archive, Company) }
+          it { is_expected.to be_able_to(:publish, Company) }
+        end
+
+        context 'can not' do
+          Ability::LAWS_RESOURCES.each do |resource|
+            it { is_expected.not_to be_able_to(:archive, resource) }
+            it { is_expected.not_to be_able_to(:publish, resource) }
+          end
+
+          (Ability::TPI_RESOURCES - Ability::PUBLISHABLE_RESOURCES).each do |resource|
+            it { is_expected.not_to be_able_to(:archive, resource) }
+            it { is_expected.not_to be_able_to(:publish, resource) }
+          end
+        end
+      end
+    end
+
+    context 'when user is LAWS publisher' do
+      let(:admin_user) { build(:admin_user, role: 'publisher_laws') }
+
+      context 'when managing' do
+        context 'can' do
+          Ability::LAWS_RESOURCES.each do |resource|
+            it { is_expected.to be_able_to(:crud, resource) }
+          end
+
+          it { is_expected.to be_able_to(:crud, Tag) }
+          it { is_expected.to be_able_to(:create, AdminUser) }
+        end
+
+        context 'can not' do
+          Ability::TPI_RESOURCES.each do |resource|
+            it { is_expected.not_to be_able_to(:crud, resource) }
+          end
+        end
+      end
+
+      context 'when archiving / publishing' do
+        context 'can' do
+          it { is_expected.to be_able_to(:archive, Legislation) }
+          it { is_expected.to be_able_to(:publish, Legislation) }
+          it { is_expected.to be_able_to(:archive, Litigation) }
+          it { is_expected.to be_able_to(:publish, Litigation) }
+          it { is_expected.to be_able_to(:archive, Target) }
+          it { is_expected.to be_able_to(:publish, Target) }
+        end
+
+        context 'can not' do
+          Ability::TPI_RESOURCES.each do |resource|
+            it { is_expected.not_to be_able_to(:archive, resource) }
+            it { is_expected.not_to be_able_to(:publish, resource) }
+          end
+
+          (Ability::LAWS_RESOURCES - Ability::PUBLISHABLE_RESOURCES).each do |resource|
+            it { is_expected.not_to be_able_to(:archive, resource) }
+            it { is_expected.not_to be_able_to(:publish, resource) }
+          end
         end
       end
     end
