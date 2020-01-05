3408f85109b54060986a71269223ed1cb1e0ecdf
Marek Kowalcze <marek.kowalcze@gmail.com>
2020-01-05 19:39:02 +0100

    update roles which publishers can select when managing other users

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -49,9 +49,7 @@ ActiveAdmin.register AdminUser do
           f.input :email
           f.input :first_name
           f.input :last_name
-          f.input :role,
-                  as: :select,
-                  collection: array_to_select_collection(AdminUser::ROLES, :titleize)
+          f.input :role, as: :select, collection: user_roles_select_collection
 
           if f.object.new_record?
             f.input :password
--- app/helpers/select_helper.rb
+++ app/helpers/select_helper.rb
@@ -9,6 +9,22 @@ module SelectHelper
     end
   end
 
+  def user_roles_select_collection
+    array_to_select_collection(current_user_roles_to_select, :titleize)
+  end
+
+  # returns roles which current user can set when creating/editing other users
+  def current_user_roles_to_select
+    case current_admin_user.role
+    when 'super_user'
+      AdminUser::ROLES
+    when 'publisher_laws'
+      %w[publisher_laws editor_laws]
+    when 'publisher_tpi'
+      %w[publisher_tpi editor_tpi]
+    end
+  end
+
   def all_languages_to_select_collection
     ::Language.common.map { |language| [language.name, language.iso_639_1] }
   end
