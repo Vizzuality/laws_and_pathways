58e18119df0c4c8b2889e04fe1002271996d7c0c
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2020-01-15 15:10:37 +0000

    Fix the url to connected entity

--- app/decorators/litigation_side_decorator.rb
+++ app/decorators/litigation_side_decorator.rb
@@ -7,6 +7,12 @@ class LitigationSideDecorator < Draper::Decorator
     model.name
   end
 
+  def frontend_name
+    return h.link_to model.name, connected_entity_frontend_url if connected_entity.present?
+
+    model.name
+  end
+
   def party_type
     model.party_type&.humanize
   end
@@ -20,5 +26,9 @@ class LitigationSideDecorator < Draper::Decorator
   def connected_entity_url
     return h.admin_company_path(connected_entity) if connected_entity.is_a?(Company)
     return h.admin_geography_path(connected_entity) if connected_entity.is_a?(Geography)
+  end
+
+  def connected_entity_frontend_url
+    return h.cclow_geography_path(connected_entity) if connected_entity.is_a?(Geography)
   end
 end
