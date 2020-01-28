85a40a51c971a2632fa53e6bd171612a3b3c4808
Simao Belchior <simaobelchior@gmail.com>
2020-01-28 08:56:02 +0100

    replaced validation with removing optional

--- app/models/litigation.rb
+++ app/models/litigation.rb
@@ -105,7 +105,7 @@ class Litigation < ApplicationRecord
   tag_with :keywords
   tag_with :responses
 
-  belongs_to :geography, optional: true
+  belongs_to :geography
   has_and_belongs_to_many :laws_sectors
   has_many :litigation_sides, -> { order(:side_type) }, inverse_of: :litigation
   has_many :documents, as: :documentable, dependent: :destroy
@@ -119,7 +119,7 @@ class Litigation < ApplicationRecord
     accepts_nested_attributes_for :events
   end
 
-  validates_presence_of :title, :slug, :document_type, :geography
+  validates_presence_of :title, :slug, :document_type
 
   def should_generate_new_friendly_id?
     title_changed? || super
