3596f6261e0b5d0d542fb8121c68ddfbf0f16c26
Tomasz <tsubik@gmail.com>
2020-02-11 22:29:48 +0100

    company chart selector

---
+++ app/javascript/components/charts/cp-performance/ChartCompanySelector.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import PropTypes from 'prop-types';
+
+function ChartCompanySelector({ companies }) {
+  return (
+    <div className="chart-company-selector">
+      <p>
+        Add up to 10 companies simultaneously
+      </p>
+
+      <div>
+      </div>
+    </div>
+  );
+}
+
+export default ChartCompanySelector;
