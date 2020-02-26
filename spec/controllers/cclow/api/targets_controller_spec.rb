bbf82fd6fc25e8e14536e977500030ec0d9812bb
Simao Belchior <simaobelchior@gmail.com>
2020-02-26 18:49:21 +0000

    Updates format to match the right format

--- spec/controllers/cclow/api/targets_controller_spec.rb
+++ spec/controllers/cclow/api/targets_controller_spec.rb
@@ -4,9 +4,12 @@ RSpec.describe CCLOW::Api::TargetsController, type: :controller do
   let!(:geography) { create(:geography, iso: 'ABC') }
   let!(:geography2) { create(:geography, iso: 'DEF') }
   let!(:geography3) { create(:geography, iso: 'GHI') }
-  let!(:target1) { create(:target, year: 2025, geography_id: geography.id) }
-  let!(:target2) { create(:target, year: 2020, geography_id: geography.id) }
-  let!(:target3) { create(:target, geography_id: geography2.id) }
+  let!(:sector1) { create(:laws_sector) }
+  let!(:sector2) { create(:laws_sector) }
+  let!(:sector3) { create(:laws_sector) }
+  let!(:target1) { create(:target, year: 2025, geography_id: geography.id, sector: sector1) }
+  let!(:target2) { create(:target, year: 2020, geography_id: geography.id, sector: sector1) }
+  let!(:target3) { create(:target, geography_id: geography2.id, sector: sector2) }
 
   describe 'Get index with geography iso as param' do
     subject { get :index, params: {iso: geography.iso} }
@@ -16,9 +19,10 @@ RSpec.describe CCLOW::Api::TargetsController, type: :controller do
     it('should return all targets for that country') do
       subject
       result = JSON.parse(response.body)
-      expect(result.size).to eq(2)
-      expect(result.first['iso_code3']).to eq(geography.iso)
-      expect(result.first['sector']).to eq(target2.sector.name)
+      expect(result['targets'].size).to eq(2)
+      expect(result['targets'].first['iso_code3']).to eq(geography.iso)
+      expect(result['targets'].first['sector']).to eq(target2.sector.name.downcase)
+      expect(result['sectors'].size).to eq(3)
     end
   end
 
@@ -30,7 +34,8 @@ RSpec.describe CCLOW::Api::TargetsController, type: :controller do
     it('should return no targets') do
       subject
       result = JSON.parse(response.body)
-      expect(result.size).to eq(0)
+      expect(result['targets'].size).to eq(0)
+      expect(result['sectors'].size).to eq(3)
     end
   end
 
