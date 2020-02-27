7936206261c6f70f90f4941510443622f023df94
Simao Belchior <simaobelchior@gmail.com>
2020-02-27 10:18:59 +0000

    annotates models

--- app/models/cp/unit.rb
+++ app/models/cp/unit.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: cp_units
+#
+#  id          :bigint           not null, primary key
+#  sector_id   :bigint
+#  valid_since :date
+#  unit        :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class CP::Unit < ApplicationRecord
   belongs_to :sector, class_name: 'TPISector', foreign_key: 'sector_id'
 
--- app/models/tpi_sector_cluster.rb
+++ app/models/tpi_sector_cluster.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: tpi_sector_clusters
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class TPISectorCluster < ApplicationRecord
   has_many :sectors, class_name: 'TPISector', foreign_key: :cluster_id, inverse_of: :cluster
 
