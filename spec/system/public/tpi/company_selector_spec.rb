3db5072e8b2b5ca6d651ba81188a5735d8167eeb
Tomasz <tsubik@gmail.com>
2020-03-15 13:54:26 +0100

    company selector specs

--- spec/system/public/tpi/company_selector_spec.rb
+++ spec/system/public/tpi/company_selector_spec.rb
@@ -6,8 +6,18 @@ describe 'Company Selector', type: 'system' do
   end
 
   it 'filters by company' do
+    click_button 'Filter by company'
+    find('.dropdown-selector__header').click
+    find('input.dropdown-selector__input').set('Renault')
+    find('input.dropdown-selector__input').native.send_keys(:return)
+    expect(page).to have_current_path('/tpi/companies/renault')
   end
 
   it 'filters by sector' do
+    click_button 'Filter by sector'
+    find('.dropdown-selector__header').click
+    find('input.dropdown-selector__input').set('Cement')
+    find('input.dropdown-selector__input').native.send_keys(:return)
+    expect(page).to have_current_path('/tpi/sectors/cement')
   end
 end
