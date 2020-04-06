77d19f4c901a29d7b9ed05d7a9724b1a952b62f7
Andrew Kuleshov <andrew.kuleshov@roonyx.team>
2020-04-06 15:14:22 +0300

    implement mobile style to publications page

--- app/javascript/components/Filters.js
+++ app/javascript/components/Filters.js
@@ -59,7 +59,9 @@ const Filters = ({ tags, sectors, resultsSize }) => {
       .map(s => encodeURIComponent(s.name))
       .join(', ');
     refreshPublicationsHtml(`tags=${activeTagsQueryParam}&sectors=${activeSectorsQueryParam}`);
-  }, [activeTags, activeSectors]);
+
+    document.getElementById('filter-button').addEventListener('click', handleButtonClick);
+  }, [activeTags, activeSectors, handleButtonClick]);
 
   const handleTagClick = (tag) => {
     const otherOptions = optionsWithoutALL(activeTags);
@@ -107,13 +109,13 @@ const Filters = ({ tags, sectors, resultsSize }) => {
     <div className={cx('filters__wrapper-margin', { 'filters__wrapper-background': isFilterOpen })}>
       <div className="container publications">
         <div className="filters__wrapper">
-          <div className="">
+          <div className="showing-count">
             <p>Showing <strong>{resultsCount}</strong> items in <strong>All Publications and news</strong></p>
           </div>
-          <div className="filters__button">
+          <div className="filters__button is-hidden-touch">
             <button
               type="button"
-              className={cx('button is-centered', { 'filters__button-active': !isFilterOpen, 'filters__button-notactive': isFilterOpen})}
+              className={cx('button is-centered is-hidden-touch', { 'filters__button-active': !isFilterOpen, 'filters__button-notactive': isFilterOpen})}
               onClick={handleButtonClick}
             >
               <img
