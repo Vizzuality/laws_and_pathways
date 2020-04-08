3595d5c985f377a3af2d2ba2bf8c303e16f0f6ce
Andrew Kuleshov <andrew.kuleshov@roonyx.team>
2020-04-08 11:41:10 +0300

    fix tpi - pages design

--- app/models/content.rb
+++ app/models/content.rb
@@ -15,6 +15,8 @@ class Content < ApplicationRecord
   belongs_to :page
   has_many :images, dependent: :destroy
 
+  CONTENT_TYPES = %w[regular text_description].freeze
+
   with_options allow_destroy: true, reject_if: :all_blank do
     accepts_nested_attributes_for :images
   end
