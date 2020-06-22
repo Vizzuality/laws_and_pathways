484a38c450240256038cec883d072565ac7948b0
Simao Belchior <simaobelchior@gmail.com>
2020-06-22 15:44:29 +0100

    Fetches home content from TPI Page. Adds task to generate current page content

--- app/controllers/tpi/home_controller.rb
+++ app/controllers/tpi/home_controller.rb
@@ -10,16 +10,8 @@ module TPI
       end
 
       @partners_logos = TPIPage.find_by(slug: 'research-funding-partners')&.contents&.flat_map(&:images)
-
-      publications = Publication.where.not(publication_date: nil)
-        .order(publication_date: :desc).limit(3)
-      news = NewsArticle.where.not(publication_date: nil)
-        .order(publication_date: :desc).limit(3)
-
-      @publications_and_articles = (publications + news)
-        .sort { |a, b| b.publication_date <=> a.publication_date }[0, 3]
-
-      @home_content = TPTIPage.find_by(slug: 'home')
+      @publications_and_articles = publications_and_articles
+      @home_content = home_content
 
       fixed_navbar('Dashboard', admin_root_path)
     end
@@ -31,5 +23,43 @@ module TPI
     def register; end
 
     def disclaimer; end
+
+    private
+
+    def publications_and_articles
+      publications = Publication.where.not(publication_date: nil)
+        .order(publication_date: :desc).limit(3)
+      news = NewsArticle.where.not(publication_date: nil)
+        .order(publication_date: :desc).limit(3)
+
+      (publications + news).sort do |a, b|
+        b.publication_date <=> a.publication_date
+      end.first(3)
+    end
+
+    def home_content
+      page = TPIPage.find_by(slug: 'homepage-content')
+      {
+        intro: {
+          title: page&.contents&.first&.title || 'The TPI tool',
+          description: ActionView::Base.full_sanitizer.sanitize(page&.contents&.first&.text) ||
+            'The Transition Pathway Initiative (TPI) is a global, asset-owner led
+              initiative which assesses companies\' preparedness for the transition to
+              a low carbon economy. Rapidly becoming the go-to corporate climate action benchmark,
+              the TPI tool is available here.'
+        },
+        investors: {
+          title: page&.contents&.second&.title || 'How investors can use the TPI',
+          description: ActionView::Base.full_sanitizer.sanitize(page&.contents&.second&.text) ||
+            'The TPI is designed to support investors. Find out how they can use its findings.'
+        },
+        supporters: {
+          title: page&.contents&.third&.title || 'Supporters',
+          description: ActionView::Base.full_sanitizer.sanitize(page&.contents&.third&.text) ||
+            'The TPI is supported globally by more than 75 investors with over
+              $20.5 trillion combined Assets Under Management and Advice.'
+        }
+      }
+    end
   end
 end
