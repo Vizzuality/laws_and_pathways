a279ae18935ee3c4262eb27c9e2b915fc2c30966
Simao Belchior <simaobelchior@gmail.com>
2020-06-25 11:57:34 +0100

    show fields in order

--- app/controllers/tpi/home_controller.rb
+++ app/controllers/tpi/home_controller.rb
@@ -38,24 +38,24 @@ module TPI
     end
 
     def home_content
-      page = TPIPage.find_by(slug: 'homepage-content')
+      contents = Content.joins(:page).where(pages: {slug: 'homepage-content'}).order(:created_at)
       {
         intro: {
-          title: page&.contents&.first&.title || 'The TPI tool',
-          description: ActionView::Base.full_sanitizer.sanitize(page&.contents&.first&.text) ||
+          title: contents&.first&.title || 'The TPI tool',
+          description: ActionView::Base.full_sanitizer.sanitize(contents&.first&.text) ||
             'The Transition Pathway Initiative (TPI) is a global, asset-owner led
               initiative which assesses companies\' preparedness for the transition to
               a low carbon economy. Rapidly becoming the go-to corporate climate action benchmark,
               the TPI tool is available here.'
         },
         investors: {
-          title: page&.contents&.second&.title || 'How investors can use the TPI',
-          description: ActionView::Base.full_sanitizer.sanitize(page&.contents&.second&.text) ||
+          title: contents&.second&.title || 'How investors can use the TPI',
+          description: ActionView::Base.full_sanitizer.sanitize(contents&.second&.text) ||
             'The TPI is designed to support investors. Find out how they can use its findings.'
         },
         supporters: {
-          title: page&.contents&.third&.title || 'Supporters',
-          description: ActionView::Base.full_sanitizer.sanitize(page&.contents&.third&.text) ||
+          title: contents&.third&.title || 'Supporters',
+          description: ActionView::Base.full_sanitizer.sanitize(contents&.third&.text) ||
             'The TPI is supported globally by more than 75 investors with over
               $20.5 trillion combined Assets Under Management and Advice.'
         }
