fa360e740d9a0a6610c731ff1156cba36608a13e
Simao Belchior <simaobelchior@gmail.com>
2020-07-16 09:45:25 +0100

    adds rack-cors to manage cors

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,17 @@ module LawsAndPathways
     config.generators.system_tests = nil
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource(
+          '/cclow/api/*',
+          headers: :any,
+          methods: [:get, :options]
+        )
+      end
+    end
   end
 
   def self.credentials
