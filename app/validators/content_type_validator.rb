5a129f10593582eb3d3896138127896d54cc13ea
Simao Belchior <simaobelchior@gmail.com>
2020-07-23 15:26:15 +0100

    adds more explicit message to content type validator

--- app/validators/content_type_validator.rb
+++ app/validators/content_type_validator.rb
@@ -2,7 +2,9 @@ class ContentTypeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if !value.attached? || content_types.include?(value.content_type)
 
-    record.errors.add(attribute, 'has wrong content type', options)
+    msg = "has wrong content type, found: #{value.content_type}, was expecting: "\
+      "#{content_types.join(' or ')}"
+    record.errors.add(attribute, msg, options)
   end
 
   private
