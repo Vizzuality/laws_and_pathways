32b4092d21fceb38de96a7f77e6b7d696760c2b4
Tomasz <tsubik@gmail.com>
2020-07-30 17:11:45 +0200

    truncate breadcrumb title

--- app/models/site/breadcrumb.rb
+++ app/models/site/breadcrumb.rb
@@ -1,3 +1,7 @@
 module Site
-  Breadcrumb = Struct.new(:title, :path)
+  Breadcrumb = Struct.new(:title, :path) do
+    def short_title
+      ActionController::Base.helpers.truncate(title, length: 100)
+    end
+  end
 end
