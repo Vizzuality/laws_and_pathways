c99811a64db6afce1bf01b6a49713c18675fa3a5
Tomasz <tsubik@gmail.com>
2020-08-28 14:01:56 +0200

    fix message verifier active storage error - just temp fix

--- app/helpers/file_helper.rb
+++ app/helpers/file_helper.rb
@@ -1,6 +1,7 @@
 module FileHelper
   def preview_file_tag(file, options = {})
     return unless file.attached?
+    return unless file.persisted? # TODO: remove when using direct upload to uploading file
 
     link_to "Uploaded file: #{file.blob.filename}", rails_blob_path(file), options.merge(target: '_blank')
   end
