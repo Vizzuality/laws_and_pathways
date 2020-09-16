679b36bb15d957a2cee301d20baf88d79a5f37c0
Tomasz <tsubik@gmail.com>
2020-09-16 14:56:35 +0200

    set logrotation

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -83,6 +83,8 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.logger = Logger.new(config.paths['log'].first, 5, 100.megabytes)
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
