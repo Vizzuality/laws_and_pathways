375fd48597c7238b9ffedf6a2ce188df32580a08
Tomasz <tsubik@gmail.com>
2020-09-22 10:20:12 +0200

    get path and title for custom pages

--- app/controllers/concerns/tpi/static_pages_controller.rb
+++ app/controllers/concerns/tpi/static_pages_controller.rb
@@ -7,10 +7,14 @@ module TPI
     end
 
     def static_pages
-      @tpi_tool_pages = TPIPage.where(menu: 'tpi_tool')
-      @tpi_tool_pages_paths = @tpi_tool_pages.map(&:path)
-      @about_pages = TPIPage.where(menu: 'about')
-      @about_pages_paths = @about_pages.map(&:path)
+      @tpi_tool_pages = TPIPage.where(menu: 'tpi_tool').map do |p|
+        {path: p.path, title: p.title}
+      end
+      @tpi_tool_pages_paths = @tpi_tool_pages.map { |p| p[:path] }
+      @about_pages = TPIPage.where(menu: 'about').map do |p|
+        {path: p.path, title: p.title}
+      end
+      @about_pages_paths = @about_pages.map { |p| p[:path] }
     end
   end
 end
