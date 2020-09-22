ccf9083d17b938e19b8566f6cf6a454574f949eb
Tomasz <tsubik@gmail.com>
2020-09-22 21:01:38 +0200

    fix target import: ghg target, add more specs for target imporer

--- app/services/csv_import/targets.rb
+++ app/services/csv_import/targets.rb
@@ -46,7 +46,7 @@ module CSVImport
       {
         target_type: row[:target_type]&.downcase&.gsub(' ', '_'),
         description: row[:description],
-        ghg_target: (row[:ghg_target] == 'ghg'),
+        ghg_target: row[:ghg_target],
         year: row[:year],
         base_year_period: row[:base_year_period],
         single_year: row[:single_year],
