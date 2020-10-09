c33e46f29c61f8269c06126642a4869cf1e0b886
Tomasz <tsubik@gmail.com>
2020-10-09 14:26:19 +0200

    do not show unpublished news and articles

--- spec/services/queries/tpi/news_publications_query_spec.rb
+++ spec/services/queries/tpi/news_publications_query_spec.rb
@@ -1,30 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Queries::TPI::NewsPublicationsQuery do
-  let(:keyword_1) { create(:keyword, name: 'keyword1') }
-  let(:keyword_2) { create(:keyword, name: 'keyword2') }
-  let(:sector_1) { create(:tpi_sector, name: 'sector1') }
-  let(:sector_2) { create(:tpi_sector, name: 'sector2') }
+  let_it_be(:keyword_1) { create(:keyword, name: 'keyword1') }
+  let_it_be(:keyword_2) { create(:keyword, name: 'keyword2') }
+  let_it_be(:sector_1) { create(:tpi_sector, name: 'sector1') }
+  let_it_be(:sector_2) { create(:tpi_sector, name: 'sector2') }
 
-  let!(:news_1) {
+  let_it_be(:news_1) {
     create(
       :news_article,
+      :published,
       keywords: [
         keyword_1,
         keyword_2
       ]
     )
   }
-  let!(:news_2) {
+  let_it_be(:news_2) {
     create(
       :news_article,
+      :published,
       keywords: [
         keyword_1
       ]
     )
   }
-  let!(:publication_1) { create(:publication, tpi_sectors: [sector_1], keywords: [keyword_1]) }
-  let!(:publication_2) { create(:publication, tpi_sectors: [sector_2]) }
+  let_it_be(:news_3) {
+    create(
+      :news_article,
+      keywords: [
+        keyword_1,
+        keyword_2
+      ],
+      publication_date: 3.days.from_now
+    )
+  }
+  let_it_be(:publication_1) { create(:publication, :published, tpi_sectors: [sector_1], keywords: [keyword_1]) }
+  let_it_be(:publication_2) { create(:publication, :published, tpi_sectors: [sector_2]) }
+  let_it_be(:publication_3) {
+    create(:publication, tpi_sectors: [sector_1], keywords: [keyword_1], publication_date: 3.days.from_now)
+  }
 
   subject { described_class }
 
