dae93a2c5a5e76cbd26b916e62aeda4274b2364c
Tomasz <tsubik@gmail.com>
2020-11-12 16:09:26 +0100

    more generic sitemap urls

--- app/controllers/cclow/sitemaps_controller.rb
+++ app/controllers/cclow/sitemaps_controller.rb
@@ -1,9 +1,7 @@
 module CCLOW
   class SitemapsController < CCLOWController
     def index
-      domain = Rails.configuration.try(:cclow_domain)
-      host = domain.start_with?('www.') ? domain : "www.#{domain}"
-      @host = "https://#{host}"
+      @host = "https://#{request.host_with_port}"
       @geographies = ::Geography.published.select(:id, :slug)
       @laws_by_geo_id = Legislation.laws.published.select(:id, :slug, :geography_id).group_by(&:geography_id)
       @policies_by_geo_id = Legislation.policies.published.select(:id, :slug, :geography_id).group_by(&:geography_id)
--- app/controllers/tpi/sitemaps_controller.rb
+++ app/controllers/tpi/sitemaps_controller.rb
@@ -1,9 +1,7 @@
 module TPI
   class SitemapsController < TPIController
     def index
-      domain = Rails.configuration.try(:tpi_domain)
-      host = domain.start_with?('www.') ? domain : "www.#{domain}"
-      @host = "https://#{host}"
+      @host = "https://#{request.host_with_port}"
       @sectors = TPISector.order(:name)
       @companies = Company.published.select(:id, :slug).order(:name)
       @publications = Publication.select(:id)
