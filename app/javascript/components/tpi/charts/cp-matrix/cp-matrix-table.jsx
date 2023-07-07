1a7d181e0d8f2581bc2250e2de66caccff5c4cd9
Alvaro Leal <pursuance@gmail.com>
2023-07-07 17:24:51 +0200

    Style fixes.Add scroll shadows. Update icon

--- app/javascript/components/tpi/charts/cp-matrix/cp-matrix-table.jsx
+++ app/javascript/components/tpi/charts/cp-matrix/cp-matrix-table.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable operator-linebreak */
 /* eslint-disable function-paren-newline */
 /* eslint-disable implicit-arrow-linebreak */
 import React from 'react';
@@ -5,13 +6,14 @@ import PropTypes from 'prop-types';
 import Table from 'rc-table';
 import ReactTooltip from 'react-tooltip';
 import cx from 'classnames';
-import chevronIcon from 'images/icon_chevron_dark/chevron_down_black-1.svg';
+import chevronIcon from 'images/icon-go-to-arrow.svg';
+import { useScrollClasses } from './cp-matrix-table-hooks';
 
 const COLORS = {
   'Not assessable using TPI’s methodology': { color: '#CACBCE' },
   'No or unsuitable disclosure': { color: 'white', border: true },
   'Not Aligned': { color: '#ED3D4A' },
-  'National Pledges': { color: '#F6C242' },
+  'National Pledges': { color: '#F99602' },
   'Below 2 Degrees': { color: '#F9DF65' },
   '1.5 Degrees': { color: '#57BE77' }
 };
@@ -38,19 +40,40 @@ ColorDot.propTypes = {
 };
 
 function CPMatrixTable({ data, meta }) {
+  useScrollClasses();
+
   if (!data) return <div>no data</div>;
+
+  const shortenTitle = (title) => {
+    const truncateString = (str, maxLength) => {
+      if (str.length <= maxLength) {
+        return str;
+      }
+      const words = str.slice(0, maxLength).split(' ');
+      return `${words.slice(0, words.length - 1).join(' ')}...`;
+    };
+
+    if (title.length > 28) {
+      return <div title={title}>{truncateString(title, 32)}</div>;
+    }
+    return title;
+  };
+
   const href = window.location.pathname;
   const { portfolios } = meta || {};
+
   const columns = [
     {
       title: 'Bussiness Segment',
+      className: 'vertical-align-center',
       children: [
         {
           title: 'Activities',
           dataIndex: 'activities',
           key: 'activities',
+          className: 'activities-column',
           width: 200,
-          ellipsis: true,
+          fixed: true,
           render: (content) => {
             const assumptions = data[content]?.assumptions;
             const hasEmissionsChart = data[content]?.has_emissions;
@@ -76,12 +99,19 @@ function CPMatrixTable({ data, meta }) {
   ].concat(
     Object.entries(portfolios).map(([portfolio, sectors]) => ({
       title: portfolio,
-      children: sectors.map((sector) => ({
-        title: sector,
+      className: 'vertical-align-center',
+      children: sectors.map((sector, i) => ({
+        title: shortenTitle(sector),
         dataIndex: sector,
         key: sector,
-        width: 200,
-        ellipsis: true,
+        width: 120,
+        className:
+          // eslint-disable-next-line no-nested-ternary
+          sectors.length > 1
+            ? i !== sectors.length - 1
+              ? 'sector-column-dashed-left'
+              : 'sector-column-dashed-right'
+            : '',
         render: (content) => <ColorDot value={content} />
       }))
     }))
@@ -98,9 +128,8 @@ function CPMatrixTable({ data, meta }) {
       <Table
         columns={columns}
         data={parsedData}
-        className="bordered"
-        scroll={{ y: false, x: 200 }}
-        ellipsis
+        className="cp-matrix-table bordered"
+        scroll={{ y: false, x: 800 }}
       />
       <div className="cp-matrix-legend">
         {Object.keys(COLORS).map((key) => (
--- app/javascript/components/tpi/charts/cp-performance/options.js
+++ app/javascript/components/tpi/charts/cp-performance/options.js
@@ -98,24 +98,6 @@ export function getOptions({ chartData, unit }) {
   };
 }
 
-export function getMultipleOptions({ chartData, unit }) {
-  return {
-    ...getOptions({ chartData, unit }),
-    chart: { height: 300, width: 400},
-    yAxis: {
-      title: {
-        text: unit,
-        reserveSpace: false,
-        textAlign: 'left',
-        align: 'low',
-        rotation: -90,
-        x: 0,
-        y: 10
-      }
-    }
-  };
-}
-
 export function getMobileOptions({ chartData, unit }) {
   const descktopOptions = getOptions({ chartData, unit });
   return merge({}, descktopOptions, {
@@ -148,6 +130,24 @@ export function getMobileOptions({ chartData, unit }) {
       }
     }
   });
+}
+
+export function getMultipleOptions({ chartData, unit }) {
+  return {
+    ...getOptions({ chartData, unit }),
+    chart: { height: 300, width: 400},
+    yAxis: {
+      title: {
+        text: unit,
+        reserveSpace: false,
+        textAlign: 'left',
+        align: 'low',
+        rotation: -90,
+        x: -20,
+        y: 10
+      }
+    }
+  };
 }
 
 export function getMultipleMobileOptions({ chartData, unit }) {
