7c3d8973ae5e401fff9ef9a842f9b331cf39cf7e
barbara-chaves <barbarabarcellos90@gmail.com>
2023-10-31 09:35:23 +0100

    Change countries sizes range

--- app/javascript/components/tpi/charts/ascor-bubble/constants.js
+++ app/javascript/components/tpi/charts/ascor-bubble/constants.js
@@ -5,3 +5,14 @@ export const SCORE_RANGES = {
 };
 
 export const VALUES = { No: [], Partial: [], Yes: [] };
+
+// radius of bubbles scale
+export const SCALE = 1;
+
+// radius of bubbles
+export const COMPANIES_MARKET_CAP_GROUPS = {
+  xLarge: 15 * SCALE,
+  large: 12 * SCALE,
+  medium: 9 * SCALE,
+  small: 6 * SCALE
+};
