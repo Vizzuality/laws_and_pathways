8e58eb8ac9953326aae5f79c99f3da626f04a7e4
barbara-chaves <barbarabarcellos90@gmail.com>
2023-11-22 11:33:26 +0100

    Add country benchmarks chart

---
+++ app/javascript/components/tpi/charts/ascor-benchmarks/Chart.js
@@ -0,0 +1,84 @@
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import { options } from './options';
+
+const Chart = ({ data: { data, metadata } }) => {
+  const chartData = useMemo(
+    () => [
+      {
+        name: 'Country emissions pathway',
+        custom: { unit: metadata.unit },
+        data: Object.entries(data.emissions).map(([year, value]) => ({
+          x: Number(year),
+          y: value
+        })),
+        zoneAxis: 'x',
+        zones: [
+          {
+            value: data.last_historical_year
+          },
+          {
+            dashStyle: 'dash'
+          }
+        ]
+      },
+      ...data?.benchmarks.map((d) => ({
+        custom: { unit: metadata.unit },
+        name: d.benchmark_type,
+        data: Object.entries(d.emissions).map(([year, value]) => ({
+          x: Number(year),
+          y: value
+        }))
+      }))
+    ],
+    [data, metadata.unit]
+  );
+
+  return (
+    <div className="ascor-benchmarks">
+      <div className="ascor-benchmarks__legend">
+        <span>
+          <span className="ascor-benchmarks__legend__line" />
+          Trend
+        </span>
+        <span>
+          <span className="ascor-benchmarks__legend__line --targeted-pathway" />
+          Targeted pathway
+        </span>
+      </div>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={{
+          ...options,
+          yAxis: {
+            ...options.yAxis,
+            title: { ...options.yAxis.title, text: metadata.unit }
+          },
+          series: chartData
+        }}
+      />
+    </div>
+  );
+};
+
+Chart.propTypes = {
+  data: PropTypes.shape({
+    data: PropTypes.shape({
+      benchmarks: PropTypes.arrayOf(
+        PropTypes.shape({
+          benchmark_type: PropTypes.string,
+          emissions: PropTypes.objectOf(PropTypes.number)
+        })
+      ),
+      emissions: PropTypes.objectOf(PropTypes.number),
+      last_historical_year: PropTypes.number
+    }),
+    metadata: PropTypes.shape({
+      unit: PropTypes.string
+    })
+  }).isRequired
+};
+
+export default Chart;
