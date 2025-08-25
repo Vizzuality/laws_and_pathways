4f38616b06091f14b8e55606e5189e5ff83a656e
mountolive <lpguercio@gmail.com>
2025-08-25 15:40:06 +0200

    Make local migration for index idempotent

--- db/migrate/20250825093802_fix_bank_assessment_indicators_uniqueness_constraint.rb
+++ db/migrate/20250825093802_fix_bank_assessment_indicators_uniqueness_constraint.rb
@@ -1,21 +1,21 @@
 class FixBankAssessmentIndicatorsUniquenessConstraint < ActiveRecord::Migration[6.1]
   def up
-    # First, ensure the old constraint exists before trying to remove it
-    if index_exists?(:bank_assessment_indicators, [:indicator_type, :number])
-      remove_index :bank_assessment_indicators, [:indicator_type, :number]
-    end
-    
-    # Add the new uniqueness constraint that includes version
-    add_index :bank_assessment_indicators, [:indicator_type, :number, :version], 
-              unique: true, 
-              name: 'index_bank_assessment_indicators_on_type_number_version'
+    # # First, ensure the old constraint exists before trying to remove it
+    # if index_exists?(:bank_assessment_indicators, [:indicator_type, :number])
+    #   remove_index :bank_assessment_indicators, [:indicator_type, :number]
+    # end
+
+    # # Add the new uniqueness constraint that includes version
+    # add_index :bank_assessment_indicators, [:indicator_type, :number, :version],
+    #           unique: true,
+    #           name: 'index_bank_assessment_indicators_on_type_number_version'
   end
 
   def down
-    # Remove the new constraint
-    remove_index :bank_assessment_indicators, name: 'index_bank_assessment_indicators_on_type_number_version'
-    
-    # Restore the old constraint
-    add_index :bank_assessment_indicators, [:indicator_type, :number], unique: true
+    # # Remove the new constraint
+    # remove_index :bank_assessment_indicators, name: 'index_bank_assessment_indicators_on_type_number_version'
+
+    # # Restore the old constraint
+    # add_index :bank_assessment_indicators, [:indicator_type, :number], unique: true
   end
 end
