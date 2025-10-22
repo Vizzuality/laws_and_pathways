e2d907e7320e24c2b114601704490b3d7535fb79
leo guercio <lpguercio@gmail.com>
2025-10-22 21:51:56 +0200

    Order admin download of assessments: ASCOR

--- app/admin/ascor/assessments.rb
+++ app/admin/ascor/assessments.rb
@@ -74,18 +74,37 @@ ActiveAdmin.register ASCOR::Assessment do
     end
     column :assessment_date
     column :publication_date
-    ASCOR::AssessmentIndicator.where.not(indicator_type: 'pillar')
-      .where.not(code: %w[EP.1.a.i EP.1.a.ii]).order(:id).all.each do |indicator|
+    pillar_order = %w[EP CP CF]
+    areas = ASCOR::AssessmentIndicator.where(indicator_type: 'area').order('length(code), code').to_a
+    indicators = ASCOR::AssessmentIndicator.where(indicator_type: 'indicator').order(:id).to_a
+    metrics = ASCOR::AssessmentIndicator.where(indicator_type: 'metric').order(:id).to_a
+
+    areas_by_pillar = areas.group_by { |a| a.code.to_s.split('.').first }
+    indicators_by_area = indicators.group_by { |i| i.code.to_s.split('.')[0..1].join('.') }
+    metrics_by_indicator = metrics.group_by { |m| m.code.to_s.split('.')[0..2].join('.') }
+
+    ordered = []
+    pillar_order.each do |pillar_key|
+      (areas_by_pillar[pillar_key] || []).each do |area|
+        ordered << area
+        (indicators_by_area[area.code.to_s] || []).each do |ind|
+          ordered << ind
+          (metrics_by_indicator[ind.code.to_s] || []).each { |m| ordered << m }
+        end
+      end
+    end
+
+    ordered.select { |i| i.indicator_type != 'pillar' && !%w[EP.1.a.i EP.1.a.ii].include?(i.code.to_s) }.each do |indicator|
       column "#{indicator.indicator_type} #{indicator.code}", humanize_name: false do |resource|
         controller.assessment_results[[resource.id, indicator.id]]&.first&.answer
       end
     end
-    ASCOR::AssessmentIndicator.where(indicator_type: %w[indicator metric]).order(:id).all.each do |indicator|
+    ordered.select { |i| i.indicator_type.in?(%w[indicator metric]) }.each do |indicator|
       column "source #{indicator.indicator_type} #{indicator.code}", humanize_name: false do |resource|
         controller.assessment_results[[resource.id, indicator.id]]&.first&.source
       end
     end
-    ASCOR::AssessmentIndicator.where(indicator_type: 'metric').order(:id).all.each do |indicator|
+    ordered.select { |i| i.indicator_type == 'metric' }.each do |indicator|
       column "year #{indicator.indicator_type} #{indicator.code}", humanize_name: false do |resource|
         controller.assessment_results[[resource.id, indicator.id]]&.first&.year
       end
