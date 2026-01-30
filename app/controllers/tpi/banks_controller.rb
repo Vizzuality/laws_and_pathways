310858735a603ccedd6ad07cb6ae3479d9cf4155
leo guercio <lpguercio@gmail.com>
2026-01-30 11:15:28 +0100

    Show state of always first: banking

--- app/controllers/tpi/banks_controller.rb
+++ app/controllers/tpi/banks_controller.rb
@@ -12,9 +12,14 @@ module TPI
     def index
       @assessment_dates = BankAssessment.dates_with_data
       banks_sector = TPISector.find_by(slug: 'banks')
-      @publications_and_articles = (banks_sector&.publications.to_a + banks_sector&.news_articles.to_a)
+      all_items = (banks_sector&.publications.to_a + banks_sector&.news_articles.to_a)
         .sort_by { |p| -p.publication_date.to_i }
-        .first(4)
+      pinned = all_items.find { |p| p.is_a?(Publication) && p.id == 206 }
+      if pinned
+        all_items.delete(pinned)
+        all_items.unshift(pinned)
+      end
+      @publications_and_articles = all_items.first(4)
       @publications_and_articles = @publications_and_articles.select { |pa| pa.publication_date <= Time.current }
       bank_page = TPIPage.find_by(slug: 'banks-content')
       @methodology_description = Content.find_by(
