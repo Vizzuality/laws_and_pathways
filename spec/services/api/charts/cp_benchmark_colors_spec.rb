1403be831944351482e5fce36e03bfa1d4c517ee
leo guercio <lpguercio@gmail.com>
2026-08-17 13:35:07 +0200

    Again color stuff

--- spec/services/api/charts/cp_benchmark_colors_spec.rb
+++ spec/services/api/charts/cp_benchmark_colors_spec.rb
@@ -2,23 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Api::Charts::CPBenchmarkColors do
   describe '.for_ordered_scenarios' do
-    it 'gives Paper scenarios three distinct colors' do
+    it 'gives the most ambitious scenario the darkest fill' do
+      colors = described_class.for_ordered_scenarios(
+        ['1.5 Degrees', 'Below 2 Degrees', 'National Pledges']
+      )
+
+      expect(colors).to eq(['#2465F5', '#5587F7', '#86A9F9'])
+    end
+
+    it 'uses the same grading for Paper scenario names' do
       colors = described_class.for_ordered_scenarios(
         ['Below 2 Degrees', '2 Degrees', 'Paris Pledges']
       )
 
       expect(colors.uniq.size).to eq(3)
-      expect(colors).to eq(['#5587F7', '#2465F5', '#86A9F9'])
-    end
-
-    it 'does not reuse a palette color when a named scenario already claimed it' do
-      colors = described_class.for_ordered_scenarios(
-        ['Below 2 Degrees', 'Unnamed scenario', 'Paris Pledges']
-      )
-
-      expect(colors.uniq.size).to eq(3)
-      expect(colors[0]).to eq('#5587F7')
-      expect(colors[1]).not_to eq('#5587F7')
+      expect(colors).to eq(['#2465F5', '#5587F7', '#86A9F9'])
     end
   end
 end
